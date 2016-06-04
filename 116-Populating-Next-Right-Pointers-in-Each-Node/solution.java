cc7ecf34b82741d9a7e8f4ee3f159c5bda4deeaf
ScottShao <neuscottshao@gmail.com>
2016-06-04 17:57:12 -0600

    [116. Populating Next Right Pointers in Each Node][Accepted]committed by LeetCode Extension

--- 116-Populating-Next-Right-Pointers-in-Each-Node/solution.java
+++ 116-Populating-Next-Right-Pointers-in-Each-Node/solution.java
@@ -13,7 +13,8 @@ public class Solution {
             for (TreeLinkNode cur = root; cur != null; cur = cur.next) {
                 if (cur.left == null) break;
                 cur.left.next = cur.right;
-                if (cur.next != null) cur.right.next = cur.next.left;
+                if (cur.next != null) 
+                cur.right.next = cur.next.left;
             }
             root = root.left;
         }
