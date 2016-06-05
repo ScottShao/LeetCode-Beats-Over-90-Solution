ee7b242cc8370c721e86d0723cdb431869969cf5
ScottShao <neuscottshao@gmail.com>
2016-06-05 17:27:36 -0600

    [126. Word Ladder II][Accepted]committed by LeetCode Extension

---
+++ 126-Word-Ladder-II/solution.java
@@ -0,0 +1,41 @@
+public class Solution {
+    public List<List<String>> findLadders(String beginWord, String endWord, Set<String> wordList) {
+        List<List<String>> ladders = new ArrayList<>();
+        Queue<List<String>> queue = new LinkedList<>();
+        List<String> list = new ArrayList<>();
+        list.add(beginWord);
+        queue.add(list);
+        int level = 0;
+        Set<String> visited = new HashSet<>();
+        visited.add(beginWord);
+        
+        while (!queue.isEmpty()) {
+            Set<String> visiting = new HashSet<>();
+            for (int i = 0, len = queue.size(); i < len; i++) {
+                List<String> top = queue.poll();
+                String last = top.get(level);
+                for (int m = 0, size = last.length(); m < size; m++) {
+                    char[] chs = last.toCharArray();
+                    for (char j = 'a'; j <= 'z'; j++) {
+                        chs[m] = j;
+                        String s = new String(chs);
+                        if (wordList.contains(s) && !visited.contains(s)) {
+                            List<String> next = new ArrayList<>(top);
+                            next.add(s);
+                            queue.add(next);
+                            visiting.add(s);
+                            if (s.equals(endWord)) {
+                                ladders.add(next);
+                            }
+                        }
+                    }
+                }
+            }
+            if (ladders.size() > 0) break;
+            level++;
+            visited.addAll(visiting);
+        }
+        
+        return ladders;
+    }
+}
