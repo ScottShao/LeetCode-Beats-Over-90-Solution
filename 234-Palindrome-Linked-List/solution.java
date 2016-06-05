70faf375125d497478e1786891ebeea58a47df75
ScottShao <neuscottshao@gmail.com>
2016-06-05 11:54:51 -0600

    [234. Palindrome Linked List][Accepted]committed by LeetCode Extension

---
+++ 234-Palindrome-Linked-List/solution.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        if (fast != null) {
+            slow = slow.next;
+        }
+        
+        ListNode prev = null;
+        ListNode crt = slow;
+        while (crt != null) {
+            ListNode temp = crt.next;
+            crt.next = prev;
+            prev = crt;
+            crt = temp;
+        }
+        
+        while (prev != null) {
+            if (prev.val != head.val) {
+                return false;
+            }
+            prev = prev.next;
+            head = head.next;
+        }
+        return true;
+    }
+}
