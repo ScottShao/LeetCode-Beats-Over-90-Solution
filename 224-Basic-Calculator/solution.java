a40da091e2f63def527ae06b220927e4df3221a8
ScottShao <neuscottshao@gmail.com>
2016-06-07 10:48:22 -0600

    [224. Basic Calculator][Accepted]committed by LeetCode Extension

---
+++ 224-Basic-Calculator/solution.java
@@ -0,0 +1,39 @@
+public class Solution {
+    public int calculate(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        char[] chs = s.toCharArray();
+        Stack<Integer> exStack = new Stack<>();
+        int sign = 1;
+        int result = 0;
+        int val = 0;
+        
+        for (char ch : chs) {
+            if (ch == ' ') {
+                continue;
+            } else if (ch >= '0' && ch <= '9') {
+                val = val * 10 + ch - '0';
+            } else if (ch == '+') {
+                result += sign * val;
+                val = 0;
+                sign = 1;
+            } else if (ch == '-') {
+                result += sign * val;
+                val = 0;
+                sign = -1;
+            } else if (ch == '(') {
+                exStack.push(result);
+                exStack.push(sign);
+                sign = 1;
+                result = 0;
+            } else {
+                result += sign * val;
+                result = result * exStack.pop() + exStack.pop();
+                val = 0;
+            }
+        }
+        
+        return result + sign * val;
+    }
+}
