705073e0afd9553583f0ea1bb044828062a51371
ScottShao <neuscottshao@gmail.com>
2016-06-07 17:14:21 -0600

    [9. Palindrome Number][Accepted]committed by LeetCode Extension

--- 9-Palindrome-Number/solution.java
+++ 9-Palindrome-Number/solution.java
@@ -1,11 +1,13 @@
 public class Solution {
     public boolean isPalindrome(int x) {
-        if (x<0 || (x!=0 && x%10==0)) return false;
-        int rev = 0;
-        while (x>rev){
-            rev = rev*10 + x%10;
-            x = x/10;
+        if (x < 0) return false;
+        int ans = 0;
+        int num = x;
+        while ( num > 0) {
+            ans = ans * 10 + num % 10;
+            num = num / 10;
         }
-        return (x==rev || x==rev/10);
+        if (ans != x) return false;
+        return true;
     }
 }
