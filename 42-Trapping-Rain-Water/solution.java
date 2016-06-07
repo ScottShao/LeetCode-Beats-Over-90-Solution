3b618f95c8b6ed38dfcc9b161ead24e317a49abf
ScottShao <neuscottshao@gmail.com>
2016-06-07 17:45:32 -0600

    [42. Trapping Rain Water][Accepted]committed by LeetCode Extension

---
+++ 42-Trapping-Rain-Water/solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int trap(int[] height){
+        int low = 0;
+        int high = height.length - 1;
+        int water = 0;
+        int leftmax = 0;
+        int rightmax = 0;
+        while(low <= high){
+            leftmax = Math.max(leftmax,height[low]);
+            rightmax = Math.max(rightmax,height[high]);
+            if(leftmax < rightmax){
+                water += (leftmax - height[low]);       // leftmax is smaller than rightmax, so the (leftmax-A[a]) water can be stored
+                low++;
+            } else {
+                water += (rightmax - height[high]);
+                high--;
+            }
+        }
+        return water;
+    }
+}
