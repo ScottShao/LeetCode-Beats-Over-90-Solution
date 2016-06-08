147396285c30b72505ba93ba72c982e6b56eb71f
ScottShao <neuscottshao@gmail.com>
2016-06-07 18:03:13 -0600

    [42. Trapping Rain Water][Accepted]committed by LeetCode Extension

--- 42-Trapping-Rain-Water/solution.java
+++ 42-Trapping-Rain-Water/solution.java
@@ -3,30 +3,28 @@ public class Solution {
         int unitSum = 0;
         int sum = 0;
         int top = 0;
-        for(int i = 0; i < height.length; i++)
-        {
+        for(int i = 0; i < height.length; i++){
             //in case of re-calculating in the next section;
-            if(top < height[i]) //collect only the higher bar;
-            {
+            if(top < height[i]) {//collect only the higher bar;
                 sum += unitSum;
                 unitSum = 0;
                 top = height[i];
             }
-            else
+            else {
                 unitSum += top - height[i];
+            }
         }
         top = 0;
         unitSum = 0; //reset;
         for(int j = height.length - 1; j >= 0; j--)
         {
-            if(top <= height[j]) //collect both the equal and higher bar;
-            {
+            if(top <= height[j]) {//collect both the equal and higher bar;
                 sum += unitSum;
                 unitSum = 0;
                 top = height[j];
-            }
-            else
+            } else {
                 unitSum += top - height[j];
+            }
         }
         return sum;
     }
