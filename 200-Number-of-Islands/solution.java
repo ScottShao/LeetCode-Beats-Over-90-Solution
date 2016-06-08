2857063e6bfd8c9694e2c12205b4eec978cfb29f
ScottShao <neuscottshao@gmail.com>
2016-06-08 15:48:28 -0600

    [200. Number of Islands][Accepted]committed by LeetCode Extension

--- 200-Number-of-Islands/solution.java
+++ 200-Number-of-Islands/solution.java
@@ -1,5 +1,4 @@
 public class Solution {
-    private final int[][] DIRECTIONS = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
     public int numIslands(char[][] grid) {
         int count = 0;
         for (int i = 0; i < grid.length; i++) {
@@ -14,11 +13,11 @@ public class Solution {
     }
     
     private void bfs(char[][] grid, int i, int j) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) return;
         if (grid[i][j] <= '0') return;
         grid[i][j] = '#';
-        for (int[] dir : DIRECTIONS) {
-            bfs(grid, i + dir[0], j + dir[1]);
-        }
+        if (i > 0) bfs(grid, i - 1, j);
+        if (i < grid.length - 1) bfs(grid, i + 1, j);
+        if (j > 0) bfs(grid, i, j - 1);
+        if (j < grid[0].length - 1) bfs(grid, i, j + 1);
     }
 }
