ff033dbf79c150f680ee28b5a7e78704d3330889
ScottShao <neuscottshao@gmail.com>
2016-06-09 17:34:38 -0600

    [169. Majority Element][Accepted]committed by LeetCode Extension

--- 169-Majority-Element/solution.java
+++ 169-Majority-Element/solution.java
@@ -3,13 +3,14 @@ public class Solution {
         int major = nums[0];
         int count = 1;
         for(int i = 1; i < nums.length;i++){
-            if (major == nums[i]){
-                count ++;
-            } else if(count == 0){
-                count ++;
-                major = nums[i];
-            } else {
+            if (major != nums[i]){
                 count--;
+                if(count == 0){
+                    count = 1;
+                    major = nums[i];
+                }
+            } else {
+                count++;
             }
         }
         return major;
