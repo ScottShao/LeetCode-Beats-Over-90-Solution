949b66730fb81bdf8d4f6f640332d2d764416b60
ScottShao <neuscottshao@gmail.com>
2016-06-11 16:23:02 -0600

    [16. 3Sum Closest][Accepted]committed by LeetCode Extension

--- 16-3Sum-Closest/solution.java
+++ 16-3Sum-Closest/solution.java
@@ -1,15 +1,15 @@
 public class Solution {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
-        int size = nums.length - 1;
+        int last = nums.length - 1;
         int dif = Integer.MAX_VALUE;
         int result = target;
-        for (int i = 0, last = nums.length - 3; i <= last; i++) {
+        for (int i = 0; i <= last - 2; i++) {
             if (nums[i] * 3 - target >= dif) {
                 break;
             }
             int low = i + 1;
-            int high = size;
+            int high = last;
             while (low < high) {
                 int crt = nums[i] + nums[low] + nums[high];
                 int newDif = crt - target;
