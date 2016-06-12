d4067863979736e0579d76d421edaa14cbab22be
ScottShao <neuscottshao@gmail.com>
2016-06-11 22:43:16 -0600

    [350. Intersection of Two Arrays II][Accepted]committed by LeetCode Extension

--- 350-Intersection-of-Two-Arrays-II/solution.java
+++ 350-Intersection-of-Two-Arrays-II/solution.java
@@ -1,29 +1,24 @@
 public class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        if (nums1 == null || nums2 == null || nums1.length == 0 || nums2.length == 0) {
-            return new int[0];
-        }
-        Map<Integer, int[]> map = new HashMap<>();
-        for (int num : nums1) {
-            int[] counter = map.get(num);
-            if (counter == null) {
-                counter = new int[]{1};
-                map.put(num, counter);
-            } else {
-                counter[0]++;
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int section[]=new int [nums1.length];
+        int index1=0,index2=0,i=0;
+        for(index1=0,index2=0;(index1<nums1.length)&& (index2<nums2.length);){
+            if(nums1[index1]==nums2[index2]){
+                section[i++]=nums1[index1];
+                index1++;
+                index2++;
+            }
+            else if(nums1[index1]<nums2[index2]){
+                index1++;
+            }
+            else{
+                index2++;
             }
         }
-        
-        int[] result = new int[Math.min(nums1.length, nums2.length)];
-        int count = 0;
-        for (int num : nums2) {
-            int[] c = map.get(num);
-            if (c != null && c[0] > 0) {
-                c[0]--;
-                result[count++] = num;
-            }
-        }
-        
-        return Arrays.copyOfRange(result, 0, count);
+        int result[]=new int[i];
+        System.arraycopy(section, 0, result, 0, i);
+        return result;
     }
 }
