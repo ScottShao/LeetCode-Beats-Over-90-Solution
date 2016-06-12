323873538085abb0741eee0ee82628910b0c999e
ScottShao <neuscottshao@gmail.com>
2016-06-12 12:46:22 -0600

    [152. Maximum Product Subarray][Accepted]committed by LeetCode Extension

--- 152-Maximum-Product-Subarray/solution.java
+++ 152-Maximum-Product-Subarray/solution.java
@@ -1,57 +1,22 @@
 public class Solution {
-    public int maxProduct(int[] A) {
-        int n = A.length;
-        if (n == 0) {
-            return 0;
-        }
-    
-        int min = A[0];
-        int max = A[0];
-        int ret = A[0];
-    
-        for (int i = 1; i < n; i++) {
-            if (min == 0 || max == 0) {
-                int temp = max;
-                max = Math.max(Math.max(max * A[i], min * A[i]), A[i]);
-                min = Math.min(Math.min(temp * A[i], min * A[i]), A[i]);
-                ret = Math.max(max, ret);
-                continue;
-            }
-    
-    
-            if (A[i] > 0) {
-                if (min > 0) {
-                    min = A[i];
-                    max *= A[i];
-                } else if (max < 0) {
-                    min *= A[i];
-                    max = A[i];
-                } else {
-                    min *= A[i];
-                    max *= A[i];
-                }
-            } else if (A[i] < 0) {
-                if (min > 0) {
-                    min = max * A[i];
-                    max = A[i];
-                } else if (max < 0) {
-                    max = min * A[i];
-                    min = A[i];
-                } else {
-                    int temp = min;
-                    min = max * A[i];
-                    max = temp * A[i];
-                }                
+    public int maxProduct(int[] nums) {
+        if (nums.length == 0) return 0;
+        int maxProduct = nums[0];
+        int minProduct = nums[0];
+        int maxRes = nums[0];
+        for (int i = 1; i < nums.length; i++)
+        {
+            if (nums[i] >= 0)
+            {
+                maxProduct = Math.max(maxProduct * nums[i], nums[i]);
+                minProduct = Math.min(minProduct * nums[i], nums[i]);
             } else {
-                min = 0;
-                max = 0;
+                int temp = maxProduct;
+                maxProduct = Math.max(minProduct * nums[i], nums[i]);
+                minProduct = Math.min(temp * nums[i], nums[i]);
             }
-
-            if (max > ret) {
-                ret = max;
-            }
+            maxRes = Math.max(maxRes, maxProduct);
         }
-
-        return ret;
+        return maxRes;
     }
 }
