f2339425035eb526e156d3443a718fd1c0c33a81
ScottShao <neuscottshao@gmail.com>
2016-06-12 12:03:08 -0600

    [73. Set Matrix Zeroes][Accepted]committed by LeetCode Extension

--- 73-Set-Matrix-Zeroes/solution.java
+++ 73-Set-Matrix-Zeroes/solution.java
@@ -1,39 +1,20 @@
 public class Solution {
     public void setZeroes(int[][] matrix) {
-        boolean is1stRow = false;
-        boolean is1stCol = false;
-        
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (matrix[i][j] == 0) {
-                    if (i == 0) is1stRow = true;
-                    else matrix[0][j] = 0;
-                    if (j == 0) is1stCol = true;
-                    else matrix[i][0] = 0;
-                    
-                }
-            }
-        }
-        
-        for (int i = 1; i < matrix.length; i++) {
-            for (int j = 1; j < matrix[0].length; j++) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+        int m = matrix.length, n = matrix[0].length, k = 0;
+        // First row has zero?
+        while (k < n && matrix[0][k] != 0) ++k;
+        // Use first row/column as marker, scan the matrix
+        for (int i = 1; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                if (matrix[i][j] == 0)
+                    matrix[0][j] = matrix[i][0] = 0;
+        // Set the zeros
+        for (int i = 1; i < m; ++i)
+            for (int j = n - 1; j >= 0; --j)
+                if (matrix[0][j] == 0 || matrix[i][0] == 0)
                     matrix[i][j] = 0;
-                }
-            }
-        }
-        
-        if (is1stCol) {
-            for (int i = 0; i < matrix.length; i++) {
-                matrix[i][0] = 0;
-            }
-        }
-        
-        if (is1stRow) {
-            for (int i = 0; i < matrix[0].length; i++) {
-                matrix[0][i] = 0;
-            }
-        }
+        // Set the zeros for the first row
+        if (k < n) Arrays.fill(matrix[0], 0);
         
     }
 }
