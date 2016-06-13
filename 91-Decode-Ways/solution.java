30e9c63364d2bb1cedb12c5fc7505f6bb7c119fb
ScottShao <neuscottshao@gmail.com>
2016-06-13 14:55:08 -0600

    [91. Decode Ways][Accepted]committed by LeetCode Extension

--- 91-Decode-Ways/solution.java
+++ 91-Decode-Ways/solution.java
@@ -1,29 +1,32 @@
 public class Solution {
     public int numDecodings(String s) {
-        if (s == null || s.length() == 0 || s.startsWith("0")) {
+        if (s == null || s.length() == 0) {
             return 0;
         }
         char[] chs = s.toCharArray();
-        int[] nums = new int[chs.length + 1];
         int prev = chs[0] - '0';
-        nums[0] = 1;
-        nums[1] = 1;
+        if (prev == 0) {
+            return 0;
+        }
+        int prevNum = 1;
+        int prepreNum = 1;
         for (int i = 2; i <= chs.length; i++) {
             int crt = chs[i - 1] - '0';
-            
+            int temp = prevNum;
             if (crt == 0) {
                 if (prev > 2 || prev == 0) {
+                    prevNum = 0;
                     break;
                 }
-                nums[i] = nums[i - 2];
+                prevNum = prepreNum;
             } else {
-                nums[i] = nums[i - 1];
                 if (prev == 1 || (prev == 2 && crt <= 6)) {
-                    nums[i] += nums[i - 2];
+                    prevNum += prepreNum;
                 }
             }
             prev = crt;
+            prepreNum = temp;
         }
-        return nums[chs.length];
+        return prevNum;
     }
 }
