97bdc7067610a9fa3e6ed622802fdf6508769958
ScottShao <neuscottshao@gmail.com>
2016-06-14 16:24:26 -0600

    [131. Palindrome Partitioning][Accepted]committed by LeetCode Extension

--- 131-Palindrome-Partitioning/solution.java
+++ 131-Palindrome-Partitioning/solution.java
@@ -10,34 +10,22 @@ public class Solution {
         for (int i = 0; i < len; i++)
             for (int j = 0; j < len - i; j++)
                 p[j][j + i] = value[j] == value[j + i] && (i <= 1 || p[j + 1][j + i - 1]);
-        search(s.toCharArray(), 0, new ArrayList<String>(), results, new HashMap<Integer, Set<Integer>>(), p);
+        search(s.toCharArray(), 0, new ArrayList<String>(), results, p);
         return results;
     }
     
-    private void search(char[] value, int start, List<String> crt, List<List<String>> results, Map<Integer, Set<Integer>> memo, boolean[][] p) {
+    private void search(char[] value, int start, List<String> crt, List<List<String>> results, boolean[][] p) {
         if (start == value.length) {
             List<String> temp = new ArrayList<>(crt);
             results.add(temp);
         } else {
-            Set<Integer> set = memo.get(start);
             int len = crt.size();
-            if (set != null) {
-                for (int idx : set) {
-                    crt.add(new String(value, start, idx - start));
-                    search(value, idx, crt, results, memo, p);
+            for (int j = start; j < value.length; j++) {
+                if (p[start][j]) {
+                    crt.add(new String(value, start, j - start + 1));
+                    search(value, j + 1, crt, results, p);
                     crt.remove(len);
                 }
-            } else {
-                set = new HashSet<>();
-                for (int j = start; j < value.length; j++) {
-                    if (p[start][j]) {
-                        set.add(j + 1);
-                        crt.add(new String(value, start, j - start + 1));
-                        search(value, j + 1, crt, results, memo, p);
-                        crt.remove(len);
-                    }
-                }
-                memo.put(start, set);
             }
         }
     }
