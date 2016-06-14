a66712704c80cae90d0fb35262c1a26ab63691e6
ScottShao <neuscottshao@gmail.com>
2016-06-13 20:01:28 -0600

    [198. House Robber][Accepted]committed by LeetCode Extension

--- 198-House-Robber/solution.java
+++ 198-House-Robber/solution.java
@@ -1,9 +1,12 @@
 public class Solution {
     public int rob(int[] nums) {
-        int[] sum = new int[nums.length + 2];
-        for (int i = 2; i < nums.length + 2; i++) {
-            sum[i] = Math.max(sum[i - 1], sum[i - 2] + nums[i - 2]);
+        int first = 0;
+        int second = 0;
+        for (int num : nums) {
+            int temp = second;
+            second = Math.max(first + num, second);
+            first = temp;
         }
-        return sum[nums.length + 1];
+        return second;
     }
 }
