2955c520b0dd0338e31e2dc4ee92edc2f57de34b
ScottShao <neuscottshao@gmail.com>
2016-06-14 23:37:03 -0600

    [34. Search for a Range][Accepted]committed by LeetCode Extension

---
+++ 34-Search-for-a-Range/solution.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] re = new int[]{-1, -1};
+        if (nums == null || nums.length == 0) {
+            return re;
+        }
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int mid = low + ((high - low) >>> 1);
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        if (nums[low] != target) {
+            return re;
+        } 
+        re[0] = low;
+        high = nums.length - 1;
+        while (low < high) {
+            int mid = low + ((high - low) >>> 1);
+            if (nums[mid] <= target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        if (nums[low] != target) {
+            low--;
+        }
+        re[1] = low;
+        return re;
+    }
+}
