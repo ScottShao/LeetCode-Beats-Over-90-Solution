47afbd358a4864556510fb91daa31dd65f339592
ScottShao <neuscottshao@gmail.com>
2016-06-15 20:25:32 -0600

    [279. Perfect Squares][Accepted]committed by LeetCode Extension

--- 279-Perfect-Squares/solution.java
+++ 279-Perfect-Squares/solution.java
@@ -1,15 +1,15 @@
 public class Solution {
     public int numSquares(int n) {
-        if(n==0) return 0;
-        for(int i=0; i*i<=n; i++){
-            int j=(int)Math.sqrt(n-i*i);
-            if(i*i+j*j==n){
-               if(i==0||j==0) return 1;
+        if(n == 0) return 0;
+        for(int i = 0; i * i <= n; i++){
+            int j = (int)Math.sqrt(n - i*i);
+            if(i * i + j * j == n){
+               if(i == 0 || j == 0) return 1;
                else return 2;
             } 
         }
-        while(n%4==0) n /= 4;
-        if(n%8==7) return 4;
+        while(n % 4 == 0) n /= 4;
+        if(n % 8 == 7) return 4;
         return 3;
     }
 }
