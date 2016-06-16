d09a318d10f449d368377b69290e4e3acb1c97d9
ScottShao <neuscottshao@gmail.com>
2016-06-15 22:04:06 -0600

    [221. Maximal Square][Accepted]committed by LeetCode Extension

--- 221-Maximal-Square/solution.java
+++ 221-Maximal-Square/solution.java
@@ -11,7 +11,12 @@ public class Solution {
         for(int i = 1; i < m; ++i) {
             for(int j = 1; j < n; ++j){
                 if(matrix[i][j] == '1'){
-                        dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i - 1][j - 1], dp[i][j-1])) + 1; 
+                    int L1 = dp[i][j - 1];
+                    int L2 = dp[i - 1][j];
+                    if(L1 != L2)
+                        dp[i][j] = Math.min(L1, L2) + 1;
+                    else
+                        dp[i][j] = Math.min(L1, dp[i - 1][j - 1]) + 1; 
                 }
             }
         }
