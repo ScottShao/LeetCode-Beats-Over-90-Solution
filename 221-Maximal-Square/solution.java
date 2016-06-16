4423c25871d9f76d7b91ec90fa396fa2329d647d
ScottShao <neuscottshao@gmail.com>
2016-06-15 21:57:45 -0600

    [221. Maximal Square][Accepted]committed by LeetCode Extension

--- 221-Maximal-Square/solution.java
+++ 221-Maximal-Square/solution.java
@@ -1,20 +1,30 @@
 public class Solution {
     public int maximalSquare(char[][] matrix) {
-        if (matrix.length == 0 || matrix[0].length == 0) {
-            return 0;
-        }
         int m = matrix.length;
+        if(m <= 0)return 0;
         int n = matrix[0].length;
-        int[][] square = new int[m + 1][n + 1];
-        int max = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == '1') {
-                    square[i + 1][j + 1] = Math.min(square[i][j], Math.min(square[i + 1][j], square[i][j + 1])) + 1;
-                    max = Math.max(square[i + 1][j + 1], max);
+        int[][] dp = new int[m][n];
+        for(int i = 0; i < m; ++i)
+            dp[i][0] = matrix[i][0] - '0';
+        for(int j = 0; j < n; ++j)
+            dp[0][j] = matrix[0][j] - '0';
+        for(int i = 1; i < m; ++i)
+            for(int j = 1; j < n; ++j){
+                if(matrix[i][j] == '0'){
+                    dp[i][j] = 0;
+                }else{
+                    int L1 = dp[i][j - 1];
+                    int L2 = dp[i - 1][j];
+                    if(L1 != L2)
+                        dp[i][j] = Math.min(L1, L2) + 1;
+                    else
+                        dp[i][j] = (matrix[i-L1][j-L1] == '1') ? L1 + 1 : L1;            
                 }
             }
-        }
+        int max = 0;
+        for(int[] row : dp)
+            for(int col : row)
+                max = Math.max(max, col);
         return max * max;
     }
 }
