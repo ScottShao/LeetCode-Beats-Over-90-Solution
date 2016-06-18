1d4b1790d9de915897df653d13e63511d5ef3db4
ScottShao <neuscottshao@gmail.com>
2016-06-18 12:09:11 -0600

    [215. Kth Largest Element in an Array][Accepted]committed by LeetCode Extension

---
+++ 215-Kth-Largest-Element-in-an-Array/solution.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        int low = 0;
+        int high = nums.length - 1;
+        int index = nums.length - k;
+        while (true) {
+            int pivot = nums[high];
+            int smallerNumIdx = low;
+            int largerNumIdx = high;
+            int crt = low;
+            while (crt <= largerNumIdx) {
+                if (nums[crt] < pivot) {
+                    swap(nums, crt++, smallerNumIdx++);
+                } else if (nums[crt] > pivot) {
+                    swap(nums, crt, largerNumIdx--);
+                } else {
+                    crt++;
+                }
+            }
+            if (index > largerNumIdx) low = largerNumIdx + 1;
+            else if (index < smallerNumIdx) high = smallerNumIdx - 1;
+            else return pivot;
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
