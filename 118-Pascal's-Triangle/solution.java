c3dae682cadfe73758fa328bfc955b3f8a2cd7bb
ScottShao <neuscottshao@gmail.com>
2016-06-19 15:37:53 -0600

    [118. Pascal's Triangle][Accepted]committed by LeetCode Extension

--- 118-Pascal's-Triangle/solution.java
+++ 118-Pascal's-Triangle/solution.java
@@ -4,18 +4,16 @@ public class Solution {
         if (numRows == 0) {
             return results;
         }
-        List<Integer> one = new ArrayList(){{
-            add(1);
-        }};
-        results.add(one);
-        for (int i = 1; i < numRows; i++) {
-            List<Integer> prev = results.get(i - 1);
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> prev = i == 0 ? null : results.get(i - 1);
             List<Integer> crt = new ArrayList<>();
-            crt.add(1);
-            for (int j = 1; j < i; j++) {
-                crt.add(prev.get(j) + prev.get(j - 1));
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    crt.add(1);
+                } else {
+                    crt.add(prev.get(j) + prev.get(j - 1));
+                }
             }
-            crt.add(1);
             results.add(crt);
         }
         return results;
