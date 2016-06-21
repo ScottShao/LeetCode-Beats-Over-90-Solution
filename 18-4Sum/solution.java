f8ea659828bad996b20b48f7f66541c8cc43b97d
ScottShao <neuscottshao@gmail.com>
2016-06-20 20:37:50 -0600

    [18. 4Sum][Accepted]committed by LeetCode Extension

--- 18-4Sum/solution.java
+++ 18-4Sum/solution.java
@@ -2,44 +2,52 @@ public class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> results = new ArrayList<>();
         int n = nums.length;
-        if (n == 0) {
+        if (n < 4) {
             return results;
         }
         Arrays.sort(nums);
+        int prev = Integer.MAX_VALUE;
         for (int i = 0; i < n - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
+            if (nums[i] == prev) {
                 continue;
             }
             if (nums[i] * 4 > target) {
                 break;
             }
+            prev = nums[i];
+            int prev2 = Integer.MAX_VALUE;
             int dif = target - nums[i];
             for (int j = i + 1; j < n - 2; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                if (nums[j] == prev2) {
                     continue;
                 }
                 if (nums[j] * 3 > dif) {
                     break;
                 }
+                prev2 = nums[j];
                 int newDif = dif - nums[j];
                 int low = j + 1;
                 int high = n - 1;
                 if (nums[low] * 2 > newDif) {
                     break;
                 }
+                int prevLow = Integer.MAX_VALUE;
+                int prevHigh = Integer.MIN_VALUE;
                 while (low < high) {
-                    if ((low > j + 1 && nums[low] == nums[low - 1])) {
+                    if (nums[low] == prevLow) {
                         low++;
                         continue;
                     }
-                    if (high < n -1 && nums[high] == nums[high + 1]) {
+                    if (nums[high] == prevHigh) {
                         high--;
                         continue;
                     }
                     int sum = nums[low] + nums[high];
                     if (sum > newDif) {
+                        prevHigh = nums[high];
                         high--;
                     } else if (sum < newDif) {
+                        prevLow = nums[low];
                         low++;
                     } else {
                         List<Integer> one = new ArrayList<>();
@@ -48,6 +56,8 @@ public class Solution {
                         one.add(nums[low]);
                         one.add(nums[high]);
                         results.add(one);
+                        prevLow = nums[low];
+                        prevHigh = nums[high];
                         low++;
                         high--;
                     }
