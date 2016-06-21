2ddd4decb34cc7a578dbcb8012fa1d963a395884
ScottShao <neuscottshao@gmail.com>
2016-06-21 11:40:00 -0600

    [220. Contains Duplicate III][Accepted]committed by LeetCode Extension

--- 220-Contains-Duplicate-III/solution.java
+++ 220-Contains-Duplicate-III/solution.java
@@ -1,19 +1,45 @@
 public class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        if (k < 1 || t < 0) return false;
-        Map<Long, Long> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            long remappedNum = (long) nums[i] - Integer.MIN_VALUE;
-            long bucket = remappedNum / ((long) t + 1);
-            if (map.containsKey(bucket)
-                    || (map.containsKey(bucket - 1) && remappedNum - map.get(bucket - 1) <= t)
-                        || (map.containsKey(bucket + 1) && map.get(bucket + 1) - remappedNum <= t))
-                            return true;
-            if (map.entrySet().size() >= k) {
-                long lastBucket = ((long) nums[i - k] - Integer.MIN_VALUE) / ((long) t + 1);
-                map.remove(lastBucket);
+        //trivial reject
+        if (nums.length < 2) return false;
+        if (k == 0 || t < 0) return false;
+
+        int max = nums[0];
+        int min = nums[0];
+
+        for (int i : nums)
+        {
+            max = Math.max(i, max);
+            min = Math.min(i, min);
+        }
+
+        // trivial accept
+        if (max == min) return true;
+
+        double temp = t;
+        temp++;
+
+        int length = (int)Math.ceil(((double)max-min)/temp) + 1;
+        int[][] table = new int[3][length];
+
+        for (int i = 0; i < nums.length; i++)
+        {
+            int num = nums[i];
+            int place =(int) (((double)num - min)/temp);
+
+            if (table[0][place] != 0)
+            {
+                if (table[1][place] + k >= i)
+                    return true;
+                else if (place > 0 && table[0][place - 1] == 1 && table[1][place - 1] + k >= i && table[2][place - 1] + t >= num)
+                    return true;
+                else if (place < length - 1 && table[0][place + 1] == 1 && table[1][place + 1] + k >= i && table[2][place + 1] - t <= num)
+                    return true;
             }
-            map.put(bucket, remappedNum);
+
+            table[0][place] = 1;
+            table[1][place] = i;
+            table[2][place] = num;
         }
         return false;
     }
