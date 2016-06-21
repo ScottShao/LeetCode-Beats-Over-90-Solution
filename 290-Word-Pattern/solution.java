966e74cfd6f288d2219139c83e5d460e7336c659
ScottShao <neuscottshao@gmail.com>
2016-06-20 22:05:38 -0600

    [290. Word Pattern][Accepted]committed by LeetCode Extension

--- 290-Word-Pattern/solution.java
+++ 290-Word-Pattern/solution.java
@@ -1,20 +1,11 @@
 public class Solution {
     public boolean wordPattern(String pattern, String str) {
-        Map<Character, String> chToStr = new HashMap<>();
-        Map<String, Character> strToCh = new HashMap<>();
-        String[] words = str.split(" ");
-        char[] val = pattern.toCharArray();
-        int n = val.length;
-        if (n != words.length) {
-            return false;
-        }
-        for (int i = 0; i < n; i++) {
-            String oldStr = chToStr.put(val[i], words[i]);
-            if (oldStr != null && !oldStr.equals(words[i])) {
-                return false;
-            }
-            Character oldCh = strToCh.put(words[i], val[i]);
-            if (oldCh != null && oldCh != val[i]) {
+        String[] strs = str.split(" ");
+        int length = pattern.length();
+        if(strs.length != length) return false;
+        Map<String,Integer> map = new HashMap<String,Integer>();
+        for(Integer i = 0;i<length;i++){
+            if(map.put(pattern.charAt(i)+"*",i) != map.put(strs[i],i)){
                 return false;
             }
         }
