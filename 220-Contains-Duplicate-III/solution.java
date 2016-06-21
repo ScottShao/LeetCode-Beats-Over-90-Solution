9b4b81b662587c44e0225039a0e3fbe06944aceb
ScottShao <neuscottshao@gmail.com>
2016-06-21 11:36:12 -0600

    [220. Contains Duplicate III][Accepted]committed by LeetCode Extension

--- 220-Contains-Duplicate-III/solution.java
+++ 220-Contains-Duplicate-III/solution.java
@@ -1,17 +1,19 @@
 public class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
         if (k < 1 || t < 0) return false;
-
-        TreeSet<Integer> set = new TreeSet<>();
+        Map<Long, Long> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            int c = nums[i];
-            if (set.floor(c) != null && set.floor(c) >= c - t
-             || set.ceiling(c) != null && set.ceiling(c) <= c + t) {
-                return true;
-            } else {
-                set.add(c);
-                if (i >= k)  set.remove(nums[i - k]);
+            long remappedNum = (long) nums[i] - Integer.MIN_VALUE;
+            long bucket = remappedNum / ((long) t + 1);
+            if (map.containsKey(bucket)
+                    || (map.containsKey(bucket - 1) && remappedNum - map.get(bucket - 1) <= t)
+                        || (map.containsKey(bucket + 1) && map.get(bucket + 1) - remappedNum <= t))
+                            return true;
+            if (map.entrySet().size() >= k) {
+                long lastBucket = ((long) nums[i - k] - Integer.MIN_VALUE) / ((long) t + 1);
+                map.remove(lastBucket);
             }
+            map.put(bucket, remappedNum);
         }
         return false;
     }
