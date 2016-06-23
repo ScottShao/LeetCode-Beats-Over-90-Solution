f59520b91bad09db6a39e6a8919e4a7a480cd90d
ScottShao <neuscottshao@gmail.com>
2016-06-22 22:07:26 -0600

    [240. Search a 2D Matrix II][Accepted]committed by LeetCode Extension

--- 240-Search-a-2D-Matrix-II/solution.java
+++ 240-Search-a-2D-Matrix-II/solution.java
@@ -1,31 +1,16 @@
 public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
-            return false;
-        }
-        return searchMatrix(matrix, target, 0, 0, matrix.length - 1, matrix[0].length - 1);
+        int line=0;
+        int len=matrix.length;
+        int col=matrix[0].length;
+        while (line<len && col>0){
+            int num= matrix[line][col-1];
+            if (num>target) col--;
+            if (num<target) line++;
+            if (num==target) return true;}
+    
+        return false;
     }
     
-    public boolean searchMatrix(int[][] matrix, int target, int i, int j, int n, int m) {
-        if (i > n || j > m) return false;
-        if (target > matrix[n][m]) return false;
-        if (target < matrix[i][j]) return false;
-        int low = 0;
-        int high = Math.min(n - i, m - j);
-        while (low < high) {
-            int mid = low + ((high - low + 1) >>> 1);
-            int iIdx = i + mid;
-            int jIdx = j + mid;
-            if (matrix[iIdx][jIdx] > target) {
-                high = mid - 1;
-            } else if (matrix[iIdx][jIdx] < target) {
-                low = mid;
-            } else {
-                return true;
-            }
-        }
-        int nextI = i + low;
-        int nextJ = j + low;
-        return matrix[nextI][nextJ] == target || searchMatrix(matrix, target, i, nextJ + 1, nextI, m) || searchMatrix(matrix, target, nextI + 1, j, n, nextJ);
-    }
+
 }
