00df34a10d968bde202dcf97a2b0383c436cb303
ScottShao <neuscottshao@gmail.com>
2016-06-27 21:34:27 -0600

    [58. Length of Last Word][Accepted]committed by LeetCode Extension

--- 58-Length-of-Last-Word/solution.java
+++ 58-Length-of-Last-Word/solution.java
@@ -1,16 +1,7 @@
 public class Solution {
     public int lengthOfLastWord(String s) {
-        if (s == null || s.length() == 0) {
-            return 0;
-        }
-        char[] vals = s.toCharArray();
-        int last = vals.length - 1;
-        while (last >= 0 && vals[last] == ' ') last--;
-        for (int i = last; i >= 0; i--) {
-            if (vals[i] == ' ') {
-                return last - i;
-            }
-        }
-        return last + 1;
+        s = s.trim();
+        int lastIndex = s.lastIndexOf(' ') + 1;
+        return s.length() - lastIndex;
     }
 }
