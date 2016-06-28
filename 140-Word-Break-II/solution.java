984438fc1f438cff0aca658b4c8f40ba6c9cdfb6
ScottShao <neuscottshao@gmail.com>
2016-06-28 14:20:17 -0600

    [140. Word Break II][Accepted]committed by LeetCode Extension

--- 140-Word-Break-II/solution.java
+++ 140-Word-Break-II/solution.java
@@ -1,35 +1,42 @@
 public class Solution {
     public List<String> wordBreak(String s, Set<String> wordDict) {
-        List<String> results = new ArrayList<>();
-        if (s == null || s.length() == 0) {
-            return results;
-        }
-        char[] vals = s.toCharArray();
         int maxLen = 0;
         for (String word : wordDict) maxLen = Math.max(maxLen, word.length());
-        dfs(vals, 0, maxLen, new StringBuilder(), wordDict, new HashSet<>(), results);
-        return results;
+        return dfs(s.toCharArray(), 0, maxLen, wordDict, new HashSet<>(), new HashMap<>());
     }
     
-    private boolean dfs(char[] vals, int index, int maxLen, StringBuilder sb, Set<String> wordDict, Set<Integer> notQualified, List<String> results) {
+    private List<String> dfs(char[] vals, int index, int maxLen, Set<String> wordDict, Set<Integer> notQualified, Map<Integer, List<String>> wordsMap) {
         if (index >= vals.length) {
-            results.add(sb.toString().trim());
-            return true;
+            return null;
         }
-        int sbLen = sb.length();
+        List<String> found = wordsMap.get(index);
+        if (found != null) {
+            return found;
+        }
+        found = new ArrayList<>();
         boolean isPathFound = false;
         for (int i = index; i <= index + maxLen && i < vals.length; i++) {
             String temp = new String(vals, index, i - index + 1);
             if (wordDict.contains(temp)) {
                 if (!notQualified.contains(i + 1)) {
-                    sb.append(temp).append(" ");
-                    isPathFound = dfs(vals, i + 1, maxLen, sb, wordDict, notQualified, results) || isPathFound;
-                    sb.setLength(sbLen);
+                    List<String> next = dfs(vals, i + 1, maxLen, wordDict, notQualified, wordsMap);
+                    if (next == null || next.size() > 0) {
+                        isPathFound = true;
+                        if (next == null) {
+                            found.add(temp);
+                        } else {
+                            for (String n : next) {
+                                found.add(temp + " " + n);
+                            }
+                        }
+                    }
+                    
                 }
             }
             
         }
+        wordsMap.put(index, found);
         if (!isPathFound) notQualified.add(index);
-        return isPathFound;
+        return found;
     }
 }
