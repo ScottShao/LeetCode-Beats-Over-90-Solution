2966f0c3fefe731efbeb472f364b34c5abf2131d
ScottShao <neuscottshao@gmail.com>
2016-06-28 12:47:00 -0600

    [140. Word Break II][Accepted]committed by LeetCode Extension

--- 140-Word-Break-II/solution.java
+++ 140-Word-Break-II/solution.java
@@ -5,24 +5,26 @@ public class Solution {
             return results;
         }
         char[] vals = s.toCharArray();
+        int maxLen = 0;
+        for (String word : wordDict) maxLen = Math.max(maxLen, word.length());
         Map<Integer, List<String>> wordsMap = new HashMap<>();
-        dfs(vals, 0, new StringBuilder(), wordDict, wordsMap, new HashSet<>(), results);
+        dfs(vals, 0, maxLen, new StringBuilder(), wordDict, wordsMap, new HashSet<>(), results);
         return results;
     }
     
-    private boolean dfs(char[] vals, int index, StringBuilder sb, Set<String> wordDict, Map<Integer, List<String>> wordsMap, Set<Integer> notQualified, List<String> results) {
+    private boolean dfs(char[] vals, int index, int maxLen, StringBuilder sb, Set<String> wordDict, Map<Integer, List<String>> wordsMap, Set<Integer> notQualified, List<String> results) {
         if (index >= vals.length) {
             results.add(sb.toString().trim());
             return true;
         }
         int sbLen = sb.length();
         boolean isPathFound = false;
-        for (int i = index; i < vals.length; i++) {
+        for (int i = index; i <= index + maxLen && i < vals.length; i++) {
             String temp = new String(vals, index, i - index + 1);
             if (wordDict.contains(temp)) {
                 if (!notQualified.contains(i + 1)) {
                     sb.append(temp).append(" ");
-                    boolean isFound = dfs(vals, i + 1, sb, wordDict, wordsMap, notQualified, results);
+                    boolean isFound = dfs(vals, i + 1, maxLen, sb, wordDict, wordsMap, notQualified, results);
                     isPathFound = isPathFound || isFound;
                     sb.setLength(sbLen);
                 }
