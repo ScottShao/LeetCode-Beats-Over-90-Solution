a414e2180e32636d483b9f3c11ec1a091e1f2c1d
ScottShao <neuscottshao@gmail.com>
2016-06-28 20:27:10 -0600

    [135. Candy][Accepted]committed by LeetCode Extension

--- 135-Candy/solution.java
+++ 135-Candy/solution.java
@@ -11,17 +11,14 @@ public class Solution {
                 left[i] = left[i - 1] + 1;
             }
         }
-        
+        int candies = n + left[n - 1];
         for (int i = n - 2; i >= 0; i--) {
             if (ratings[i] > ratings[i + 1]) {
                 right[i] = right[i + 1] + 1;
             }
-        }
-        
-        int candies = n;
-        for (int i = 0; i < n; i++) {
             candies += Math.max(left[i], right[i]);
         }
+
         return candies;
     }
 }
