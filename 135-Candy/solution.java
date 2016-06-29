5b020088db80ae1b920abf0c240cbd6c7536f733
ScottShao <neuscottshao@gmail.com>
2016-06-28 20:42:32 -0600

    [135. Candy][Accepted]committed by LeetCode Extension

--- 135-Candy/solution.java
+++ 135-Candy/solution.java
@@ -5,17 +5,17 @@ public class Solution {
         for (int i = 1; i < ratings.length; i++) {
             if (ratings[i] >= ratings[i-1]) {
                 if (countDown > 0) {
-                    total += countDown*(countDown+1)/2; // arithmetic progression
+                    total += countDown * (countDown+1) / 2; // arithmetic progression
                     if (countDown >= prev) total += countDown - prev + 1;
                     countDown = 0;
                     prev = 1;
                 }
-                prev = ratings[i] == ratings[i-1] ? 1 : prev+1;
+                prev = ratings[i] == ratings[i-1] ? 1 : prev + 1;
                 total += prev;
             } else countDown++;
         }
         if (countDown > 0) { // if we were descending at the end
-            total += countDown*(countDown+1)/2;
+            total += countDown * (countDown + 1) / 2;
             if (countDown >= prev) total += countDown - prev + 1;
         }
         return total;
