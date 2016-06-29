c212c525cdefc8f97428210d49604c6383f610d1
ScottShao <neuscottshao@gmail.com>
2016-06-28 21:02:16 -0600

    [6. ZigZag Conversion][Accepted]committed by LeetCode Extension

--- 6-ZigZag-Conversion/solution.java
+++ 6-ZigZag-Conversion/solution.java
@@ -5,13 +5,15 @@ public class Solution {
         int skip1 = 2 * numRows - 2;
         int skip2 = skip1;
         char[] vals = s.toCharArray();
+        int n = vals.length;
         for (int i = 0; i < numRows; i++) {
             if (i != 0 && i != numRows - 1)   skip2 -= 2;
             int index = i;
-            while (index < s.length()) {
+            while (index < n) {
                 str.append(vals[index]);
-                if (i != 0 && i != numRows - 1 && index + skip2 < s.length())
-                    str.append(vals[index + skip2]);
+                int temp = index + skip2;
+                if (i != 0 && i != numRows - 1 &&  temp < n)
+                    str.append(vals[temp]);
                 index += skip1;
             }
         }
