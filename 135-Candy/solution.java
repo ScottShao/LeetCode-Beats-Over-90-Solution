6b3ff9b597dae4f04de8e082f0e8ce5445f2c5f2
ScottShao <neuscottshao@gmail.com>
2016-06-28 20:38:14 -0600

    [135. Candy][Accepted]committed by LeetCode Extension

--- 135-Candy/solution.java
+++ 135-Candy/solution.java
@@ -1,26 +1,23 @@
 public class Solution {
     public int candy(int[] ratings) {
-        if (ratings == null || ratings.length == 0) {
-            return 0;
+        if (ratings == null || ratings.length == 0) return 0;
+        int total = 1, prev = 1, countDown = 0;
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] >= ratings[i-1]) {
+                if (countDown > 0) {
+                    total += countDown*(countDown+1)/2; // arithmetic progression
+                    if (countDown >= prev) total += countDown - prev + 1;
+                    countDown = 0;
+                    prev = 1;
+                }
+                prev = ratings[i] == ratings[i-1] ? 1 : prev+1;
+                total += prev;
+            } else countDown++;
         }
-        int n = ratings.length;
-        int[] left = new int[n];
-        for (int i = 1; i < n; i++) {
-            if (ratings[i] > ratings[i - 1]) {
-                left[i] = left[i - 1] + 1;
-            }
+        if (countDown > 0) { // if we were descending at the end
+            total += countDown*(countDown+1)/2;
+            if (countDown >= prev) total += countDown - prev + 1;
         }
-        int candies = n + left[n - 1];
-        int prev = 0;
-        for (int i = n - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1]) {
-                prev++;
-            } else {
-                prev = 0;
-            }
-            candies += Math.max(left[i], prev);
-        }
-
-        return candies;
+        return total;
     }
 }
