427661199015a7744136eb5a23adc0f7f04c5dc5
ScottShao <neuscottshao@gmail.com>
2016-07-01 15:05:56 -0600

    [322. Coin Change][Accepted]committed by LeetCode Extension

--- 322-Coin-Change/solution.java
+++ 322-Coin-Change/solution.java
@@ -1,16 +1,13 @@
 public class Solution {
     public int coinChange(int[] coins, int amount) {
-        if (coins == null || coins.length == 0 || amount < 1) {
-            return 0;
-        }
-        int[] dp = new int[amount + 1];
+        if (amount < 1) return 0;
+        int[] dp = new int[amount + 1]; 
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
-        for (int i = 1; i <= amount; i++) {
-            for (int c : coins) {
-                int idx = i - c;
-                if (idx >= 0 && dp[idx] != Integer.MAX_VALUE) {
-                    dp[i] = Math.min(dp[i], dp[idx] + 1);
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                if (dp[i - coin] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
             }
         }
