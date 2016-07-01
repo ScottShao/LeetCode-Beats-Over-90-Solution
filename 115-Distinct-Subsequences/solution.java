7955d6064c1aab93cbde7d043a6b59a3632d08b6
ScottShao <neuscottshao@gmail.com>
2016-07-01 16:30:43 -0600

    [115. Distinct Subsequences][Accepted]committed by LeetCode Extension

--- 115-Distinct-Subsequences/solution.java
+++ 115-Distinct-Subsequences/solution.java
@@ -7,10 +7,12 @@ public class Solution {
         char[] tVals = t.toCharArray();
         for (int i = 0; i <= m; i++)
             dp[i][0] = 1;
-        for (int i = 1; i <= m; i++)
-            for (int j = 1; j <= Math.min(i, n); j++) // skip the case of i < j
+        for (int i = 1; i <= m; i++) {
+            int min = Math.min(i, n);
+            for (int j = 1; j <= min; j++) // skip the case of i < j
             // for (int j = Math.min(i, n); j >= 1; j--) // scan reversely also works
                 dp[i][j] = dp[i - 1][j] + ((sVals[i - 1] == tVals[j - 1]) ? dp[i - 1][j - 1] : 0);
+        }
         return dp[m][n];
         
     }
