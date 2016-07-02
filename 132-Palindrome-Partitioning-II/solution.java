6b2faba093e97e4850fbc2dde8952250fbeb8115
ScottShao <neuscottshao@gmail.com>
2016-07-01 23:19:19 -0600

    [132. Palindrome Partitioning II][Accepted]committed by LeetCode Extension

---
+++ 132-Palindrome-Partitioning-II/solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int minCut(String s) {
+        int n = s.length();
+        boolean[][] tab = new boolean[n][n];
+        int[] min = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+            min[i] = n - i - 1;
+            for (int j = i; j < n; j++) {
+                tab[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 2 || tab[i + 1][j - 1]);
+                if (tab[i][j]) min[i] = Math.min(min[i], j + 1 < n ? (1 + min[j + 1]) : 0);
+            }
+        }
+        return min[0];
+    }
+}
