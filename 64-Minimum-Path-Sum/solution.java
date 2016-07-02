c442b3f6dc2e5c77ce495543cad52474fdb53a69
ScottShao <neuscottshao@gmail.com>
2016-07-01 18:37:39 -0600

    [64. Minimum Path Sum][Accepted]committed by LeetCode Extension

--- 64-Minimum-Path-Sum/solution.java
+++ 64-Minimum-Path-Sum/solution.java
@@ -4,15 +4,13 @@ public class Solution {
         int n = grid[0].length;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (i == 0 && j == 0) {
-                    continue;
-                } else if (i == 0) {
+                if (i > 0 && j > 0) {
+                     grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+                } else if (i == 0 && j > 0) {
                     grid[i][j] += grid[i][j - 1];
-                } else if (j == 0) {
+                } else if (j == 0 && i > 0) {
                     grid[i][j] += grid[i - 1][j];
-                } else {
-                    grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
-                }
+                } 
             }
         }
         return grid[m - 1][n - 1];
