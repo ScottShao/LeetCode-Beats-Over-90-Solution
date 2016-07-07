a4e5b101658966b1c07a483b7ff540f29611ec65
ScottShao <neuscottshao@gmail.com>
2016-07-06 22:19:42 -0600

    [174. Dungeon Game][Accepted]committed by LeetCode Extension

--- 174-Dungeon-Game/solution.java
+++ 174-Dungeon-Game/solution.java
@@ -5,24 +5,24 @@ public class Solution {
         }
         int m = dungeon.length;
         int n = dungeon[0].length;
-        int low;
-        int high;
+        int small;
+        int large;
         if (m < n) {
-            low = m;
-            high = n;
+            small = m;
+            large = n;
         } else {
-            low = n;
-            high = m;
+            small = n;
+            large = m;
         }
-        int[] health = new int[low + 1];
+        int[] health = new int[small + 1];
         Arrays.fill(health, Integer.MAX_VALUE);
-        health[low] = 1;
-        for (int i = high - 1; i >= 0; i--) {
-            for (int j = low - 1; j >= 0; j--) {
+        health[small] = 1;
+        for (int i = large - 1; i >= 0; i--) {
+            for (int j = small - 1; j >= 0; j--) {
                 int temp = m < n ? dungeon[j][i] : dungeon[i][j];
                 health[j] = Math.max(Math.min(health[j], health[j + 1]) - temp, 1);
             }
-            health[low] = Integer.MAX_VALUE;
+            health[small] = Integer.MAX_VALUE;
         }
         return health[0];
     }
