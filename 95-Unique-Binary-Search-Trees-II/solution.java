20f68bfedcb8b5a4b5d752061a99b3c2498382a7
ScottShao <neuscottshao@gmail.com>
2016-07-08 12:41:02 -0600

    [95. Unique Binary Search Trees II][Accepted]committed by LeetCode Extension

--- 95-Unique-Binary-Search-Trees-II/solution.java
+++ 95-Unique-Binary-Search-Trees-II/solution.java
@@ -8,34 +8,35 @@
  * }
  */
 public class Solution {
-    public static List<TreeNode> generateTrees(int n) {
-        if (n == 0) return new ArrayList<>();
-		List<TreeNode>[] result = new List[n+1];
-		result[0] = new ArrayList<TreeNode>();
-		result[0].add(null);
-
-		for(int len = 1; len <= n; len++){
-			result[len] = new ArrayList<TreeNode>();
-			for(int j=0; j<len; j++){
-				for(TreeNode nodeL : result[j]){
-					for(TreeNode nodeR : result[len-j-1]){
-						TreeNode node = new TreeNode(j+1);
-						node.left = nodeL;
-						node.right = clone(nodeR, j+1);
-						result[len].add(node);
-					}
-				}
-			}
-		}
-		return result[n];
-	}
-	
-	private static TreeNode clone(TreeNode n, int offset){
-		if(n == null)
-			return null;
-		TreeNode node = new TreeNode(n.val + offset);
-		node.left = clone(n.left, offset);
-		node.right = clone(n.right, offset);
-		return node;
-	}
+    public List<TreeNode> generateTrees(int n) {
+        if(n==0) return new ArrayList<TreeNode>();
+        return generateTress(1, n);
+    }
+    
+    private List<TreeNode> generateTress(int start, int end){
+        if(start>end) {
+            List<TreeNode> list = new ArrayList<TreeNode>();
+            list.add(null);
+            return list;
+        }
+        if(start==end) {
+            List<TreeNode> list = new ArrayList<TreeNode>();
+            list.add(new TreeNode(start)); return list;
+        }
+        List<TreeNode> roots = new ArrayList<TreeNode>();
+        for(int i=start;i<=end;i++){
+            List<TreeNode> leftTrees = generateTress(start, i-1);
+            List<TreeNode> rightTrees = generateTress(i+1, end);
+            for(int j=0;j<leftTrees.size();j++){
+                for(int k=0;k<rightTrees.size();k++){
+                    TreeNode root = new TreeNode(i);
+                    root.left = leftTrees.get(j);
+                    root.right = rightTrees.get(k);
+                    roots.add(root);
+                }
+            }
+            
+        }
+        return roots;
+    }
 }
