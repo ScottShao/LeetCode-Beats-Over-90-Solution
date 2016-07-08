04b548c53842deb13d00587dae9fe866421f4ff3
ScottShao <neuscottshao@gmail.com>
2016-07-07 22:50:51 -0600

    [120. Triangle][Accepted]committed by LeetCode Extension

--- 120-Triangle/solution.java
+++ 120-Triangle/solution.java
@@ -1,22 +1,15 @@
 public class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int[] dp = new int[triangle.size()];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
-        int size = 1;
-        for (List<Integer> row : triangle) {
-            int prev = Integer.MAX_VALUE;
+        int len = triangle.size();
+        int[] dp = new int[len + 1];
+        int size = len;
+        for (int j = len - 1; j >= 0; j--) {
+            List<Integer> row = triangle.get(j);
             for (int i = 0; i < size; i++) {
-                int crt = dp[i];
-                dp[i] = Math.min(dp[i], prev) + row.get(i);
-                prev = crt;
+                dp[i] = Math.min(dp[i], dp[i + 1]) + row.get(i);
             }
-            size++;
+            size--;
         }
-        int min = Integer.MAX_VALUE;
-        for (int path : dp) {
-            min = Math.min(path, min);
-        }
-        return min;
+        return dp[0];
     }
 }
