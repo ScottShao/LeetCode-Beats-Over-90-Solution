23e3db498aa58d137014cab81542512ff2eeee35
ScottShao <neuscottshao@gmail.com>
2016-07-21 20:12:51 -0600

    [63. Unique Paths II][Accepted]committed by LeetCode Extension

--- 63-Unique-Paths-II/solution.java
+++ 63-Unique-Paths-II/solution.java
@@ -5,15 +5,18 @@ public class Solution {
         }
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
-        int[][] dp = new int[m + 1][n + 1];
-        dp[0][1] = 1;
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (obstacleGrid[i - 1][j - 1] == 0) {
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                    dp[j] = dp[j] + dp[j - 1];
+                } else {
+                    dp[j] = 0;
                 }
             }
+            dp[0] = 0;
         }
-        return dp[m][n];
+        return dp[n];
     }
 }
