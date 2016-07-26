0471143a31b04dd8d692fb655bcae10081ac84ea
ScottShao <neuscottshao@gmail.com>
2016-07-26 16:41:36 -0600

    [13. Roman to Integer][Accepted]committed by LeetCode Extension

--- 13-Roman-to-Integer/solution.java
+++ 13-Roman-to-Integer/solution.java
@@ -1,31 +1,30 @@
 public class Solution {
     private static final int[] values = {
-        1000, 900, 500, 400,
-        100, 90, 50, 40,
-        10, 9, 5, 4,
+        1000, 500, 
+        100, 50,
+        10, 5,
         1
     };
     
-    private static final String[] symbol = {
-        "M", "CM", "D", "CD",
-        "C", "XC", "L", "XL",
-        "X", "IX", "V", "IV",
-        "I"
+    private static final char[] symbol = {
+        'M', 'D',
+        'C', 'L',
+        'X', 'V',
+        'I'
     };
     
     public int romanToInt(String s) {
-        if (s == null || s.length() == 0) {
-            return 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < symbol.length; i++)
+            map.put(symbol[i], values[i]);
+        
+        int num = 0, prev = 0;
+        char[] vals = s.toCharArray();
+        for (int i = 0; i < vals.length; i++) {
+            int curr = map.get(vals[i]);
+            num += (curr > prev) ? (curr - 2 * prev) : curr;    // use subtractive rule
+            prev = curr;
         }
-        int i = 0;
-        int res = 0;
-        while (s.length() > 0) {
-            while (s.startsWith(symbol[i])) {
-                res += values[i];
-                s = s.substring(i % 2 + 1);
-            }
-            i++;
-        }
-        return res;
+        return num;
     }
 }
