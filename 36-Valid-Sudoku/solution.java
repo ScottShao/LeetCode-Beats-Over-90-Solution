9fe1b2fbbe517249b28143d718ecb4550d3b450a
ScottShao <neuscottshao@gmail.com>
2016-08-01 13:05:44 -0600

    [36. Valid Sudoku][Accepted]committed by LeetCode Extension

--- 36-Valid-Sudoku/solution.java
+++ 36-Valid-Sudoku/solution.java
@@ -1,27 +1,27 @@
 public class Solution {
     public boolean isValidSudoku(char[][] board) {
-        int[][] horizontal = new int[10][10];
-        int[][] vertical = new int[10][10];
-        int[][] square = new int[10][10];
+        boolean[][] horizontal = new boolean[10][10];
+        boolean[][] vertical = new boolean[10][10];
+        boolean[][] square = new boolean[10][10];
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 if (board[i][j] == '.') {
                     continue;
                 }
                 int num = board[i][j] - '0';
-                if (horizontal[i][num] > 0) {
+                if (horizontal[i][num]) {
                     return false;
                 }
-                horizontal[i][num]++;
-                if (vertical[j][num] > 0) {
+                horizontal[i][num] = true;
+                if (vertical[j][num]) {
                     return false;
                 }
-                vertical[j][num]++;
+                vertical[j][num] = true;
                 int idx = i / 3 * 3+ j / 3;
-                if (square[idx][num] > 0) {
+                if (square[idx][num]) {
                     return false;
                 }
-                square[idx][num]++;
+                square[idx][num] = true;
             }
         }
         return true;
