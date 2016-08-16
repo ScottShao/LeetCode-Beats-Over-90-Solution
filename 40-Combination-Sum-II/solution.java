6a406e6435b73320728db65c576d049a962fa8c5
ScottShao <neuscottshao@gmail.com>
2016-08-17 06:37:08 +0800

    [40. Combination Sum II][Accepted]committed by LeetCode Extension

---
+++ 40-Combination-Sum-II/solution.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> results = new ArrayList<>();
+        if (candidates == null || candidates.length == 0) {
+            return results;
+        }
+        Arrays.sort(candidates);
+        dfs(0, 0, target, candidates, new ArrayList<>(), results);
+        return results;
+    }
+    
+    private void dfs(int index, int sum, int target, int[] candidates, List<Integer> comb, List<List<Integer>> results) {
+        if (sum == target) {
+            results.add(new ArrayList<>(comb));
+            return;
+        }
+        int lastElemIdx = comb.size();
+        for (int i = index; i < candidates.length; i++) {
+            if (i > index && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+            if (candidates[i] > target) {
+                return;
+            }
+            int temp = sum + candidates[i];
+            if (temp > target) {
+                return;
+            }
+            comb.add(candidates[i]);
+            dfs(i + 1, temp, target, candidates, comb, results);
+            comb.remove(lastElemIdx);
+        }
+    }
+}
