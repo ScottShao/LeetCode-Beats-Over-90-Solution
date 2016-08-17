242b7408158fd372dea31a41214fdd40b200db29
ScottShao <neuscottshao@gmail.com>
2016-08-16 22:40:47 -0600

    [43. Multiply Strings][Accepted]committed by LeetCode Extension

--- 43-Multiply-Strings/solution.java
+++ 43-Multiply-Strings/solution.java
@@ -1,31 +1,22 @@
 public class Solution {
     public String multiply(String num1, String num2) {
-        if (num1 == null || num1.length() == 0 || num2 == null | num2.length() == 0) {
-            return null;
-        }
+        int m = num1.length(), n = num2.length();
+        int[] pos = new int[m + n];
         char[] vals1 = num1.toCharArray();
         char[] vals2 = num2.toCharArray();
-        int l1 = vals1.length;
-        int l2 = vals2.length;
-        if (l1 == 1 && vals1[0] == '0' || l2 == 1 && vals2[0] == '0') {
-            return "0";
-        }
-        StringBuilder sb = new StringBuilder();
-        int carry = 0;
-        for (int i = 0, len = l1 + l2 - 1; i < len; i++) {
-            int sum = carry;
-            for (int j = l1 - 1; l1 - j - 1 <= i && j >= 0; j--) {
-                int idx = i - l1 + j + 1;
-                if (idx < l2) {
-                    sum += (vals1[j] - '0') * (vals2[l2 - idx - 1] - '0');
-                }
+        for(int i = m - 1; i >= 0; i--) {
+            for(int j = n - 1; j >= 0; j--) {
+                int mul = (vals1[i] - '0') * (vals2[j] - '0'); 
+                int p1 = i + j, p2 = i + j + 1;
+                int sum = mul + pos[p2];
+    
+                pos[p1] += sum / 10;
+                pos[p2] = (sum) % 10;
             }
-            sb.append(sum % 10);
-            carry = sum / 10;
-        }
-        if (carry > 0) {
-            sb.append(carry);
-        }
-        return sb.reverse().toString();
+        }  
+        
+        StringBuilder sb = new StringBuilder();
+        for(int p : pos) if(!(sb.length() == 0 && p == 0)) sb.append(p);
+        return sb.length() == 0 ? "0" : sb.toString();
     }
 }
