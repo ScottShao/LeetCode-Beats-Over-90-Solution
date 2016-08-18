73fa32c7998f76a42570aabb1a38e37f2631554f
ScottShao <neuscottshao@gmail.com>
2016-08-18 00:32:06 -0600

    [47. Permutations II][Accepted]committed by LeetCode Extension

--- 47-Permutations-II/solution.java
+++ 47-Permutations-II/solution.java
@@ -1,33 +1,27 @@
 public class Solution {
-    private List<List<Integer>> listSet;
-    private List<Integer> list;
-    private Set<Integer> visited;
-    
-    public List<List<Integer>> permuteUnique(int[] num) {
-        listSet = new ArrayList<List<Integer>>();
-        list = new ArrayList<Integer>();
-        visited = new HashSet<>();
-        
-        Arrays.sort(num);   // Need to sort!!
-        dfs(num);
-        return listSet;
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(nums==null || nums.length==0) return res;
+        boolean[] used = new boolean[nums.length];
+        List<Integer> list = new ArrayList<Integer>();
+        Arrays.sort(nums);
+        dfs(nums, used, list, res);
+        return res;
     }
-    
-    private void dfs(int[] num) {
-        if (list.size() == num.length) {
-            /** if (!listSet.contains(list)) **/    // No need to check duplicate lists!!    
-                listSet.add(new ArrayList<Integer>(list));
-        } else {
-            for (int i = 0; i < num.length; i++) {
-                if (i > 0 && !visited.contains(i - 1) && num[i - 1] == num[i]) continue;    // skip the duplicated elements (important pruning step to increase speed)!!
-                if (!visited.contains(i)) {
-                    list.add(num[i]);
-                    visited.add(i);
-                    dfs(num);
-                    list.remove(list.size() - 1);
-                    visited.remove(i);
-                }
-            }
+
+    public void dfs(int[] nums, boolean[] used, List<Integer> list, List<List<Integer>> res){
+        if(list.size()==nums.length){
+            res.add(new ArrayList<Integer>(list));
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(used[i]) continue;
+            if(i>0 &&nums[i-1]==nums[i] && !used[i-1]) continue;
+            used[i]=true;
+            list.add(nums[i]);
+            dfs(nums,used,list,res);
+            used[i]=false;
+            list.remove(list.size()-1);
         }
     }
 }
