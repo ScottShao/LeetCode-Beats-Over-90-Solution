923f59127db86b3d57e277e3ef8f66af87e5db5a
ScottShao <neuscottshao@gmail.com>
2016-08-26 15:15:23 -0600

    [79. Word Search][Accepted]committed by LeetCode Extension

--- 79-Word-Search/solution.java
+++ 79-Word-Search/solution.java
@@ -1,11 +1,12 @@
 public class Solution {
     public boolean exist(char[][] board, String word) {
-        char[] vals = word.toCharArray();
-        int n = board.length;
-        int m = board[0].length;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (dfs(board, i, j, vals, 0)) {
+        if (word == null || word.length() == 0) {
+            return true;
+        }
+        char[] chs = word.toCharArray();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if(dfs(board, chs, 0, i, j)) {
                     return true;
                 }
             }
@@ -13,20 +14,21 @@ public class Solution {
         return false;
     }
     
-    private boolean dfs(char[][] board, int i, int j, char[] vals, int index) {
-        if (index == vals.length) {
+    private boolean dfs(char[][] board, char[] words, int idx, int x, int y) {
+        if (idx == words.length) {
             return true;
-        }
-        if (i < 0 || i == board.length || j < 0 || j == board[0].length || board[i][j] != vals[index]) {
+        } 
+        if (x < 0 || x == board.length || y < 0 || y == board[0].length) {
             return false;
         }
-        board[i][j] ^= 256;
-        index++;
-        boolean isFound = dfs(board, i - 1, j, vals, index) || dfs(board, i + 1, j, vals, index) ||
-                        dfs(board, i, j - 1, vals, index) || dfs(board, i, j + 1, vals, index);
-        board[i][j] ^= 256;
-        return isFound;
+        if (board[x][y] != words[idx]) {
+            return false;
+        }
+        board[x][y] ^= 256;
+        boolean exist = dfs(board, words, idx + 1, x, y + 1) ||
+        dfs(board, words, idx + 1, x, y - 1) || dfs(board, words, idx + 1, x + 1, y) ||
+        dfs(board, words, idx + 1, x - 1, y) ;
+        board[x][y] ^= 256;
+        return exist;
     }
-    
-    
 }
