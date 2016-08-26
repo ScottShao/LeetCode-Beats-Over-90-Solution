4e74eabc6192b4aaa17e7396d0b060b69a20b435
ScottShao <neuscottshao@gmail.com>
2016-08-26 12:33:45 -0600

    [78. Subsets][Accepted]committed by LeetCode Extension

--- 78-Subsets/solution.java
+++ 78-Subsets/solution.java
@@ -1,21 +1,17 @@
 public class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        res.add(new ArrayList<>());
-        dfs(0, nums, new ArrayList<>(), res);
-        return res;
+        List<List<Integer>> ans = new ArrayList<>();
+        if (nums == null) { return ans; }
+        Arrays.sort(nums);  // non-descending order
+        dfs(ans, nums, new ArrayList<Integer>(), 0);
+        return ans; 
     }
     
-    private void dfs(int index, int[] nums, List<Integer> crt, List<List<Integer>> res) {
-        if (index >= nums.length) {
-            return;
-        }
-        int size = crt.size();
-        for (int i = index; i < nums.length; i++) {
-            crt.add(nums[i]);
-            res.add(new ArrayList<>(crt));
-            dfs(i + 1, nums, crt, res);
-            crt.remove(size);
-        }
+    private void dfs(List<List<Integer>> ans, int[] nums, List<Integer> list, int index) {
+        if (index == nums.length) { ans.add(new ArrayList<>(list)); return; }
+        dfs(ans, nums, list, index+1);  // not pick the number at this index
+        list.add(nums[index]);
+        dfs(ans, nums, list, index+1);  // pick the number at this index
+        list.remove(list.size()-1);
     }
 }
