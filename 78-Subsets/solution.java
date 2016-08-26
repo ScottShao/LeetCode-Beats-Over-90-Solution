1893a7dd3b1a2a002577060daede6449a237bc51
ScottShao <neuscottshao@gmail.com>
2016-08-26 12:35:12 -0600

    [78. Subsets][Accepted]committed by LeetCode Extension

--- 78-Subsets/solution.java
+++ 78-Subsets/solution.java
@@ -14,12 +14,4 @@ public class Solution {
         }
         return subsets; 
     }
-    
-    private void dfs(List<List<Integer>> ans, int[] nums, List<Integer> list, int index) {
-        if (index == nums.length) { ans.add(new ArrayList<>(list)); return; }
-        dfs(ans, nums, list, index+1);  // not pick the number at this index
-        list.add(nums[index]);
-        dfs(ans, nums, list, index+1);  // pick the number at this index
-        list.remove(list.size()-1);
-    }
 }
