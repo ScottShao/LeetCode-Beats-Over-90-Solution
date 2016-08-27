38d967283ce248b883f68de451c14606165c550a
ScottShao <neuscottshao@gmail.com>
2016-08-27 12:49:30 -0600

    [89. Gray Code][Accepted]committed by LeetCode Extension

--- 89-Gray-Code/solution.java
+++ 89-Gray-Code/solution.java
@@ -10,8 +10,8 @@ public class Solution {
         for (int i = 1; i <= n; i++) {
             crt = next;
             next <<= 1;
-            for (int j = crt; j < next; j++) {
-                res.add(crt ^ res.get(next - j - 1));
+            for (int j = crt - 1; j >= 0; j--) {
+                res.add(crt ^ res.get(j));
             }
         }
         return res;
