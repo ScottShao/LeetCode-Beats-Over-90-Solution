06a71bb7f0ff156182852c1608c06db06226854d
ScottShao <neuscottshao@gmail.com>
2016-08-29 23:16:27 -0600

    [105. Construct Binary Tree from Preorder and Inorder Traversal][Accepted]committed by LeetCode Extension

--- 105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal/solution.java
+++ 105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal/solution.java
@@ -8,34 +8,23 @@
  * }
  */
 public class Solution {
-    private int pi;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if (preorder == null || preorder.length == 0) {
-            return null;
-        }
-        int len = preorder.length;
-        Map<Integer, Integer> val2Idx = new HashMap<>();
-        pi = 0;
-        for (int i = 0; i < len; i++) {
-            val2Idx.put(inorder[i], i);
-        }
-        return buildTree(preorder, inorder, 0, len - 1, val2Idx);
+        return buildTree(preorder, 0, inorder, inorder.length - 1, 0);
     }
     
-    private TreeNode buildTree(int[] preorder, int[] inorder, int ii, int ij, Map<Integer, Integer> val2Idx) {
-        if (ii == ij) {
-            return new TreeNode(inorder[ii]);
+    private TreeNode buildTree(int[] preorder, int idx, int[] inorder, int end, int start) {
+        if (idx >= preorder.length || start > end) {
+            return null;
         }
-        TreeNode root = new TreeNode(preorder[pi]);
-        int idx = val2Idx.get(preorder[pi]);
-        if (ii < idx) {
-            pi++;
-            root.left = buildTree(preorder, inorder, ii, idx - 1, val2Idx);
+        TreeNode root = new TreeNode(preorder[idx]);
+        int i;
+        for (i = end; i >= start; i--) {
+            if (preorder[idx] == inorder[i]) {
+                break;
+            }
         }
-        if (idx < ij) {
-            pi++;
-            root.right = buildTree(preorder, inorder, idx + 1, ij, val2Idx);
-        }
+        root.left = buildTree(preorder, idx + 1, inorder, i - 1, start);
+        root.right = buildTree(preorder, idx + i - start + 1, inorder, end, i+1);
         return root;
     }
 }
