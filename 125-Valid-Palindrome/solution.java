37ed6b45cfdc03d9baba9823e8aed8b5dcfae5db
ScottShao <neuscottshao@gmail.com>
2016-09-02 16:12:09 -0600

    [125. Valid Palindrome][Accepted]committed by LeetCode Extension

--- 125-Valid-Palindrome/solution.java
+++ 125-Valid-Palindrome/solution.java
@@ -1,13 +1,13 @@
 public class Solution {
     public boolean isPalindrome(String s) {
         int i = 0, j = s.length() - 1;
-        char[] vals = s.toLowerCase().toCharArray();
+        char[] vals = s.toCharArray();
         while (i < j) {
             while (i < j && !Character.isLetterOrDigit(vals[i]))  i++;
             while (i < j && !Character.isLetterOrDigit(vals[j]))  j--;
-            if (vals[i] != vals[j]) return false;
-            i++; 
-            j--;
+            if (Character.toLowerCase(vals[i]) != Character.toLowerCase(vals[j]))
+                return false;
+            i++; j--;
         }
         return true;
     }
