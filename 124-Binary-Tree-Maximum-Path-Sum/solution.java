3663946b717141b6fe2f98d67e168f235035b883
ScottShao <neuscottshao@gmail.com>
2016-09-02 15:55:33 -0600

    [124. Binary Tree Maximum Path Sum][Accepted]committed by LeetCode Extension

--- 124-Binary-Tree-Maximum-Path-Sum/solution.java
+++ 124-Binary-Tree-Maximum-Path-Sum/solution.java
@@ -10,31 +10,17 @@
 public class Solution {
     private int max;
     public int maxPathSum(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
         max = Integer.MIN_VALUE;
         helper(root);
         return max;
     }
     
     private int helper(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
+        if (root == null) return 0;
         int left = helper(root.left);
         int right = helper(root.right);
-        int re = root.val;
-        int temp = 0;
-        if (left > 0) {
-            re += left;
-            temp = left;
-        }  
-        if (right > 0) {
-            re += right;
-            temp = Math.max(temp, right);
-        }
-        max = Math.max(max, re);
-        return root.val + temp;
+        int re = root.val + Math.max(left, right);
+        max = Math.max(max, root.val + left + right);
+        return re > 0 ? re : 0;
     }
 }
