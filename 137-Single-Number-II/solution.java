bbdf3cef4f6e3733d85ed1a0a6897d0198ae8862
ScottShao <neuscottshao@gmail.com>
2016-09-04 11:38:08 -0600

    [137. Single Number II][Accepted]committed by LeetCode Extension

--- 137-Single-Number-II/solution.java
+++ 137-Single-Number-II/solution.java
@@ -1,17 +1,12 @@
 public class Solution {
     public int singleNumber(int[] nums) {
-        int[] bits = new int[32];
-        for (int num : nums) {
-            for (int i = 0; i < 32; i++) {
-                if ((num & 1) == 1) bits[i]++;
-                num = num >> 1;
-            }
+        int[] count = new int[32];  // 32-bit integer
+        int result = 0;
+        for (int i = 0; i < 32; i++) {  // 1st loop must be bit: otherwise count % 3 will not be 0 or 1
+            for (int num : nums)
+                if (((num >> i) & 1) == 1)     count[i]++;
+            result |= ((count[i] % 3) << i);
         }
-        int res = 0;
-        for (int i = 31; i >= 0; i--) {
-            res = res << 1;
-            res ^= bits[i] % 3;
-        }
-        return res;
+        return result;
     }
 }
