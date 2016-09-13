fe48fe78f33e8317170ba7167ee7d627a772f6a2
ScottShao <neuscottshao@gmail.com>
2016-09-13 10:19:28 -0600

    [201. Bitwise AND of Numbers Range][Accepted]committed by LeetCode Extension

--- 201-Bitwise-AND-of-Numbers-Range/solution.java
+++ 201-Bitwise-AND-of-Numbers-Range/solution.java
@@ -1,15 +1,11 @@
 public class Solution {
     public int rangeBitwiseAnd(int m, int n) {
-        if (m == n) return m;
-        int mask = 1;
-        while (mask <= n / 2) {
-            mask = mask << 1;
+        int x = m ^ n;
+        int s = x >> 1;
+        while (s > 0) {
+            x = x | s;
+            s >>= 1;
         }
-        int res = 0;
-        while (((m & mask) ^ (n & mask)) == 0) {
-            res += (m & mask);
-            mask = mask >> 1;
-        }
-        return res;
+        return m & n & ~x;
     }
 }
