453ef29352971c8a854a0559d682356f0240c68e
ScottShao <neuscottshao@gmail.com>
2016-09-15 13:29:11 -0600

    [158. Read N Characters Given Read4 II - Call multiple times][Accepted]committed by LeetCode Extension

--- 158-Read-N-Characters-Given-Read4-II---Call-multiple-times/solution.java
+++ 158-Read-N-Characters-Given-Read4-II---Call-multiple-times/solution.java
@@ -8,14 +8,15 @@ public class Solution extends Reader4 {
     public int read(char[] buf, int n) {
         int ptr = 0;
         while (ptr < n) {
-            if (buffPtr == 0) {
+            if (buffPtr >= buffCnt) {
+                buffPtr = 0;
                 buffCnt = read4(buff);
             }
             if (buffCnt == 0) break;
             while (ptr < n && buffPtr < buffCnt) {
                 buf[ptr++] = buff[buffPtr++];
             }
-            if (buffPtr >= buffCnt) buffPtr = 0;
+            
         }
         return ptr;
     }
