405b67fc04637b687648d6f1cc9aa975f2ad3159
ScottShao <neuscottshao@gmail.com>
2016-09-15 15:28:50 -0600

    [159. Longest Substring with At Most Two Distinct Characters][Accepted]committed by LeetCode Extension

--- 159-Longest-Substring-with-At-Most-Two-Distinct-Characters/solution.java
+++ 159-Longest-Substring-with-At-Most-Two-Distinct-Characters/solution.java
@@ -2,28 +2,20 @@ public class Solution {
     private static final int D = 2;
     public int lengthOfLongestSubstringTwoDistinct(String s) {
         if (s == null || s.length() == 0) return 0;
+        LinkedHashMap<Character, Integer> ch2Idx = new LinkedHashMap<>((int)((D + 1) / 0.75) + 1, 0.75f, true);
         char[] vals = s.toCharArray();
-        int[] chIdx = new int[256];
-        Arrays.fill(chIdx, -1);
-        int start = 0;
         int maxLen = 0;
-        int count = 0;
-        for (int i = 0; i < vals.length; i++) {
-            int idx = chIdx[vals[i]];
-            if (idx < start) count++;
-            chIdx[vals[i]] = i;
-            if (count > D) {
-               maxLen = Math.max(maxLen, i - start);
-               int newStart = i;
-               for (int j = 0; j < 256; j++) {
-                   if (chIdx[j] >= start) {
-                       newStart = Math.min(newStart, chIdx[j]);
-                   }
-               }
-               start = newStart + 1;
-               count = D;
+        int len = vals.length;
+        int start = 0;
+        for (int i = 0; i < len; i++) {
+            ch2Idx.put(vals[i], i);
+            if (ch2Idx.size() > D) {
+                Map.Entry<Character, Integer> mapEntry = ch2Idx.entrySet().iterator().next();
+                maxLen = Math.max(maxLen, i - start);
+                start = mapEntry.getValue() + 1;
+                ch2Idx.remove(mapEntry.getKey());
             }
         }
-        return Math.max(vals.length - start, maxLen);
+        return Math.max(len - start, maxLen);
     }
 }
