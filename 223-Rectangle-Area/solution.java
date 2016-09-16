09a7811a06b46f924fbe52225748ffb438b2a971
ScottShao <neuscottshao@gmail.com>
2016-09-15 21:23:39 -0600

    [223. Rectangle Area][Accepted]committed by LeetCode Extension

--- 223-Rectangle-Area/solution.java
+++ 223-Rectangle-Area/solution.java
@@ -1,11 +1,12 @@
 public class Solution {
     public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
-        if (H < B || F > D || E > C || G < A) return (C - A) * (D - B) + (G - E) * (H - F);
+        int res = (C - A) * (D - B) + (G - E) * (H - F);
+        if (H < B || F > D || E > C || G < A) return res;
         int bx, by, tx, ty;
         bx = Math.max(E, A);
         by = Math.max(B, F);
         tx = Math.min(G, C);
         ty = Math.min(D, H);
-        return (C - A) * (D - B) + (G - E) * (H - F) - (tx - bx) * (ty - by);
+        return res - (tx - bx) * (ty - by);
     }
 }
