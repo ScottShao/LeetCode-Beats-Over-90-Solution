f950c3ff27c93832954519e109d93d2a597c18c5
ScottShao <neuscottshao@gmail.com>
2016-09-19 15:53:40 -0600

    [250. Count Univalue Subtrees][Accepted]committed by LeetCode Extension

--- 250-Count-Univalue-Subtrees/solution.java
+++ 250-Count-Univalue-Subtrees/solution.java
@@ -8,29 +8,18 @@
  * }
  */
 public class Solution {
-    private int count = 0;
+    int count = 0;
+    boolean all(TreeNode root, int val) {
+        if (root == null)
+            return true;
+        if (!all(root.left, root.val) | !all(root.right, root.val))
+            return false;
+        count++;
+        return root.val == val;
+    }
+    
     public int countUnivalSubtrees(TreeNode root) {
-        count = 0;
-        helper(root);
+        all(root, 0);
         return count;
     }
-    
-    private int helper(TreeNode root) {
-        if (root == null) return 0;
-        if (root.left == null && root.right == null) {
-            count++;
-            return 1;
-        }
-        int left = helper(root.left);
-        int right = helper(root.right);
-        if (left < 0 || right < 0) {
-            return -1;
-        } else {
-            boolean re = left > 0 ? root.left.val == root.val : true;
-            re = re && (right > 0 ? root.right.val == root.val : true);
-            if (re) count++;
-            return re ? 1 : -1;
-        }
-    }
-    
 }
