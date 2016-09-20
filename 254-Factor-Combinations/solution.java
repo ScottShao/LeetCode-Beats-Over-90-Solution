858df244b08864537d0da8cbc44429ff97b546ae
ScottShao <neuscottshao@gmail.com>
2016-09-20 15:34:49 -0600

    [254. Factor Combinations][Accepted]committed by LeetCode Extension

---
+++ 254-Factor-Combinations/solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public List<List<Integer>> getFactors(int n) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (n <= 3) return result;
+        helper(n, -1, result, new ArrayList<Integer>());
+        return result; 
+    }
+    
+    public void helper(int n, int lower, List<List<Integer>> result, List<Integer> cur) {
+        if (lower != -1) {
+            cur.add(n);
+            result.add(new ArrayList<Integer>(cur));
+            cur.remove(cur.size() - 1);
+        }
+        int upper = (int) Math.sqrt(n);
+        for (int i = Math.max(2, lower); i <= upper; ++i) {
+            if (n % i == 0) {
+                cur.add(i);
+                helper(n / i, i, result, cur);
+                cur.remove(cur.size() - 1);
+            }
+        }
+    }
+}
