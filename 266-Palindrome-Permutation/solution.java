3a80d15628ef4240d0f8bce2f31e6f28ed91f1b2
ScottShao <neuscottshao@gmail.com>
2016-09-21 16:58:51 -0600

    [266. Palindrome Permutation][Accepted]committed by LeetCode Extension

--- 266-Palindrome-Permutation/solution.java
+++ 266-Palindrome-Permutation/solution.java
@@ -6,12 +6,15 @@ public class Solution {
         int R = 256;
         char[] vals = s.toCharArray();
         boolean[] map = new boolean[R];
+        int min = 0, max = R - 1;
         for (char ch : vals) {
             map[ch] = !map[ch];
+            min = Math.min(ch, min);
+            max = Math.max(ch, max);
         }
         int count = 0;
-        for (boolean b : map) {
-            if (b) count++;
+        for (int i = min; i <= max; i++) {
+            if (map[i]) count++;
             if (count > 1) return false;
         }
         return true;
