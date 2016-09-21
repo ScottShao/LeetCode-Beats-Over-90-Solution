9b818dd159e19a0d125e08dc2f0b2432a0dd4100
ScottShao <neuscottshao@gmail.com>
2016-09-21 15:26:38 -0600

    [261. Graph Valid Tree][Accepted]committed by LeetCode Extension

--- 261-Graph-Valid-Tree/solution.java
+++ 261-Graph-Valid-Tree/solution.java
@@ -1,33 +1,35 @@
 public class Solution {
-    public boolean validTree(int n, int[][] edges) {
-        if (n < 1) return false;
-        List<List<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
-        }
-        for (int[] e : edges) {
-            graph.get(e[0]).add(e[1]);
-            graph.get(e[1]).add(e[0]);
-        }
-        boolean[] visiting = new boolean[n];
-        if (dfs(-1, 0, visiting, graph) != n) return false;
-        return true;
-    }
-    
-    private int dfs(int prev, int crt, boolean[] visiting, List<List<Integer>> graph) {
-        if (visiting[crt]) return -1;
-        List<Integer> edges = graph.get(crt);
-        visiting[crt] = true;
-        int count = 0;
-        for (int next : edges) {
-            if (next != prev) {
-                int temp = dfs(crt, next, visiting, graph);
-                if (temp < 0) return -1;
-                count += temp;
+    class UnionFind {
+        private int[] parent;
+        public UnionFind(int n) {
+            parent = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
             }
         }
-        count++;
-        visiting[crt] = false;
-        return count;
+        
+        public int find(int p) {
+            int q = parent[p];
+            while (parent[q] != parent[p]) {
+                parent[p] = parent[q];
+                q = parent[q];
+            }
+            return q;
+        }
+        
+        public void union(int p, int q) {
+            parent[p] = q;
+        }
+    }
+    public boolean validTree(int n, int[][] edges) {
+        if (n < 1) return false;
+        UnionFind uf = new UnionFind(n);
+        for (int[] e : edges) {
+            int x = uf.find(e[0]);
+            int y = uf.find(e[1]);
+            if (x == y) return false;
+            uf.union(x, y);
+        }
+        return edges.length == n - 1;
     }
 }
