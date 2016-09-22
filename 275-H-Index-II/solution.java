572fbdb8fbc3915f9bc264dc7fb16d8f62a5931a
ScottShao <neuscottshao@gmail.com>
2016-09-22 17:02:23 -0600

    [275. H-Index II][Accepted]committed by LeetCode Extension

--- 275-H-Index-II/solution.java
+++ 275-H-Index-II/solution.java
@@ -1,9 +1,18 @@
 public class Solution {
     public int hIndex(int[] citations) {
+        if (citations == null) return 0;
         int len = citations.length;
-        for (int i = 0; i < len; i++) {
-            if (len - i <= citations[i]) return len - i;
+        if (len == 0) return 0;
+        int left = 0;
+        int right = len - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (len - mid <= citations[mid]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
         }
-        return 0;
+        return len - left <= citations[left] ? len - left : 0;
     }
 }
