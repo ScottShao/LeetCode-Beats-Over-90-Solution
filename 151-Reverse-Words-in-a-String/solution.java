3be6a3e06eafe3f40421608994515eb60784b637
ScottShao <neuscottshao@gmail.com>
2016-09-22 23:31:01 -0600

    [151. Reverse Words in a String][Accepted]committed by LeetCode Extension

--- 151-Reverse-Words-in-a-String/solution.java
+++ 151-Reverse-Words-in-a-String/solution.java
@@ -1,21 +1,38 @@
 public class Solution {
     public String reverseWords(String s) {
-        if(s.isEmpty())return s;
-        StringBuilder sb = new StringBuilder();
-        int endIndex = s.length();
-        int beginIndex;
-        while((beginIndex = s.lastIndexOf(' ', endIndex -1)) != -1){
-            String value = s.substring(beginIndex + 1, endIndex);
-            if(!value.isEmpty()){
-                sb.append(value).append(" ");
+        if (s == null) return s;
+        s = s.trim();
+        int len = s.length();
+        if(len == 0) return s;
+        char[] vals = s.toCharArray();
+        reverse(vals, 0, len - 1);
+        int idx = 0;
+        for (int i = 0; i < len; ) {
+            if (vals[i] != ' ') {
+                int j = i + 1;
+                while (j < len && vals[j] != ' ') j++;
+                reverse(vals, i, j - 1);
+                for (int k = i; k < j; k++) {
+                    vals[idx++] = vals[k];
+                }
+                if (idx == len) return new String(vals);
+                else vals[idx++] = ' ';
+                i = j + 1;
+            } else {
+                i++;
             }
-            endIndex = beginIndex;
         }
-        if(endIndex == 0 && sb.length() != 0){
-            sb.deleteCharAt(sb.length() -1);
-        }else{
-            sb.append(s.substring(0, endIndex));
+        
+        return new String(vals, 0, idx - 1);
+    }
+    
+    private void reverse(char[] vals, int low, int high) {
+        while (low < high) {
+            char temp = vals[low];
+            vals[low] = vals[high];
+            vals[high] = temp;
+            low++;
+            high--;
         }
-        return sb.toString();
     }
 }
