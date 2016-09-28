f702f21add3c0bd58862cc69cec798e77e585780
ScottShao <neuscottshao@gmail.com>
2016-09-28 16:05:28 -0600

    [295. Find Median from Data Stream][Accepted]committed by LeetCode Extension

--- 295-Find-Median-from-Data-Stream/solution.java
+++ 295-Find-Median-from-Data-Stream/solution.java
@@ -1,30 +1,19 @@
 public class MedianFinder {
-    private Queue<Integer> minHeap = new PriorityQueue<>();
-    private Queue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-    
+    PriorityQueue<Integer> min = new PriorityQueue();
+    PriorityQueue<Integer> max = new PriorityQueue(1000, Collections.reverseOrder());
     // Adds a number into the data structure.
     public void addNum(int num) {
-        if (minHeap.size() > 0 && num > minHeap.peek()) {
-            minHeap.add(num);
-            if (minHeap.size() > maxHeap.size() + 1) {
-                maxHeap.add(minHeap.poll());
-            }
-        } else {
-            maxHeap.add(num);
-            if (maxHeap.size() > minHeap.size() + 1) {
-                minHeap.add(maxHeap.poll());
-            }
+        max.offer(num);
+        min.offer(max.poll());
+        if (max.size() < min.size()){
+            max.offer(min.poll());
         }
-        
     }
 
     // Returns the median of current data stream
     public double findMedian() {
-        if (minHeap.size() == maxHeap.size()) {
-            return (double) (minHeap.peek() + maxHeap.peek()) / 2;
-        } else {
-            return minHeap.size() > maxHeap.size() ? minHeap.peek() : maxHeap.peek();
-        }
+        if (max.size() == min.size()) return (max.peek() + min.peek()) /  2.0;
+        else return max.peek();
     }
 };
 
