3eac56cd7da72342a47cd5acba96fa9d3bfb1bb3
ScottShao <neuscottshao@gmail.com>
2016-10-01 09:25:22 -0600

    [331. Verify Preorder Serialization of a Binary Tree][Accepted]committed by LeetCode Extension

---
+++ 331-Verify-Preorder-Serialization-of-a-Binary-Tree/solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public boolean isValidSerialization(String preorder) {
+        if (preorder == null) return false;
+        if (preorder.equals("#")) return true;
+        String[] nodes = preorder.split(",");
+        int len = nodes.length;
+        String[] stack = new String[len];
+        int index = 0;
+        int i;
+        for (i = 0; i < len; i++) {
+            String node = nodes[i];
+            if (node.equals("#")) {
+                if (index == 0) return false;
+                while (index > 0 && stack[index - 1].equals("#")) {
+                    index -= 2;
+                }
+                if (index == 0 && i < len - 1) break;
+            } 
+            stack[index++] = node;
+        }
+        return index == 1 && i == len && stack[0].equals("#");
+    }
+}
