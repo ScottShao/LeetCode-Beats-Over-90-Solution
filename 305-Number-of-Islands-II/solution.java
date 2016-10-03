f3c1d22928964addc3bb64be7214012872cdfebb
ScottShao <neuscottshao@gmail.com>
2016-10-03 12:57:09 -0600

    [305. Number of Islands II][Accepted]committed by LeetCode Extension

--- 305-Number-of-Islands-II/solution.java
+++ 305-Number-of-Islands-II/solution.java
@@ -1,56 +1,64 @@
 public class Solution {
-    public int find(int[] father, int id) {
-            int tf = father[id];
-            while (tf != father[tf]) tf = father[tf];
-            int cur = id, tmp;
-            while (father[cur] != tf) {
-                tmp = father[cur];
-                father[cur] = tf;
-                cur = tmp;
+    class UnionFind {
+        int[] parent;
+        int[] rank;
+        int count;
+        public UnionFind(int len) {
+            parent = new int[len];
+            rank = new int[len];
+            for (int i = 0; i < len; i++) {
+                parent[i] = i;
             }
-            return tf;
+            count = 0;
         }
-        public void union(int[] father, int[] sz, int id1, int id2) {
-            int tf1 = find(father, id1);
-            int tf2 = find(father, id2);
-            if (tf1 != tf2) {
-                if (sz[tf1] > sz[tf2]) {
-                    father[tf2] = tf1;
-                    sz[tf1] += sz[tf2];
-                }
-                else {
-                    father[tf1] = tf2;
-                    sz[tf2] += sz[tf1];
-                }
-            }
+        
+        public void union(int p, int q) {
+            int p1 = find(p);
+            int p2 = find(q);
+            if (p1 == p2) return;
+            if (rank[p1] > rank[p2]) {
+                parent[p2] = p1;
+                rank[p1] += rank[p2];
+            } else {
+                parent[p1] = p2;
+                rank[p2] += rank[p1];
+            } 
+            count--;
         }
-        public List<Integer> numIslands2(int m, int n, int[][] positions) {
-            if (m == 0 || n == 0) return new ArrayList<Integer>();
-            ArrayList<Integer> res = new ArrayList<Integer>();
-            int[] father = new int[m * n];
-            for (int i = 0; i < father.length; i ++) father[i] = -1;
-            int[] sz = new int[m * n];
-            int[] dr = {0, 0, -1, 1};
-            int[] dc = {-1, 1, 0, 0};
-            int r, c, nr, nc, count = 0;
-            for (int i = 0; i < positions.length; i ++) {
-                r = positions[i][0];
-                c = positions[i][1];
-                count ++;
-                father[r * n + c] = r * n + c;
-                sz[r * n + c] = 1;
-                for (int j = 0; j < 4; j ++) {
-                    nr = r + dr[j];
-                    nc = c + dc[j];
-                    if (nr >= 0 && nr < m && nc >= 0 && nc < n && father[nr * n + nc] != -1) {
-                        if (find(father, r * n + c) != find(father, nr * n + nc)) {
-                            count --;
-                            union(father, sz, r * n + c, nr * n + nc);
-                        }
-                    }
-                }
-                res.add(count);
+        
+        public int find(int p) {
+            int q = parent[p];
+            while (q != parent[q]) {
+                parent[p] = parent[q];
+                q = parent[q];
             }
-            return res;
+            return q;
         }
     }
+    
+    public List<Integer> numIslands2(int m, int n, int[][] positions) {
+        UnionFind uf = new UnionFind(m * n);
+        Set<Integer> used = new HashSet<>();
+        List<Integer> res = new ArrayList<>();
+        int count = 0;
+        for (int[] p : positions) {
+            int idx = p[0] * n + p[1];
+            uf.count++;
+            if (p[0] > 0 && used.contains(idx - n)) {
+                uf.union(idx, idx - n);
+            }
+            if (p[0] < m - 1 && used.contains(idx + n)) {
+                uf.union(idx, idx + n);
+            }
+            if (p[1] > 0 && used.contains(idx - 1)) {
+                uf.union(idx, idx - 1);
+            }
+            if (p[1] < n - 1 && used.contains(idx + 1)) {
+                uf.union(idx, idx + 1);
+            }
+            res.add(uf.count);
+            used.add(idx);
+        }
+        return res;
+    }
+}
