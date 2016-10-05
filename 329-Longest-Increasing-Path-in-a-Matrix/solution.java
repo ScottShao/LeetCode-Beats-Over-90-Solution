a04626fc32d7af209fcf5e34a5fb3a626b184838
ScottShao <neuscottshao@gmail.com>
2016-10-05 16:41:34 -0600

    [329. Longest Increasing Path in a Matrix][Accepted]committed by LeetCode Extension

--- 329-Longest-Increasing-Path-in-a-Matrix/solution.java
+++ 329-Longest-Increasing-Path-in-a-Matrix/solution.java
@@ -7,20 +7,23 @@ public class Solution {
         int max = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                max = Math.max(max, dfs(i, j, mem, matrix));
+                max = Math.max(max, dfs(i, j, mem, matrix, Integer.MAX_VALUE));
             }
         }
         return max;
     }
     
-    private int dfs(int i, int j, int[][] mem, int[][] matrix) {
+    private int dfs(int i, int j, int[][] mem, int[][] matrix, int prev) {
+        if (i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length || matrix[i][j] >= prev) {
+            return 0;
+        }
         if (mem[i][j] != 0) return mem[i][j];
         int crt = 0;
         int val = matrix[i][j];
-        if (i > 0 && matrix[i - 1][j] > val) crt = Math.max(crt, dfs(i - 1, j, mem, matrix));
-        if (i < matrix.length - 1 && matrix[i + 1][j] > val) crt = Math.max(crt, dfs(i + 1, j, mem, matrix));
-        if (j > 0 && matrix[i][j - 1] > val) crt = Math.max(crt, dfs(i, j - 1, mem, matrix));
-        if (j < matrix[0].length - 1 && matrix[i][j + 1] > val) crt = Math.max(crt, dfs(i, j + 1, mem, matrix));
+        crt = Math.max(crt, dfs(i - 1, j, mem, matrix, val));
+        crt = Math.max(crt, dfs(i + 1, j, mem, matrix, val));
+        crt = Math.max(crt, dfs(i, j - 1, mem, matrix, val));
+        crt = Math.max(crt, dfs(i, j + 1, mem, matrix, val));
         mem[i][j] = crt + 1;
         return mem[i][j];
     }
