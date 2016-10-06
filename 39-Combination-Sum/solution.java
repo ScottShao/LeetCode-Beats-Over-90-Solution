4f51224b2b338700ebb57c24c3dfe6b2a9880b59
ScottShao <neuscottshao@gmail.com>
2016-10-06 15:09:44 -0600

    [39. Combination Sum][Accepted]committed by LeetCode Extension

--- 39-Combination-Sum/solution.java
+++ 39-Combination-Sum/solution.java
@@ -1,34 +1,22 @@
 public class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> results = new ArrayList<>();
-        if (candidates == null || candidates.length == 0) {
-            return results;
-        }
+        List<List<Integer>> res = new ArrayList<>();
+        if (candidates == null) return res;
+        int len = candidates.length;
+        if (len == 0) return res;
         Arrays.sort(candidates);
-        dfs(0, 0, target, candidates, new ArrayList<>(), results);
-        return results;
+        dfs(candidates, 0, 0, target, new ArrayList<>(), res);
+        return res;
     }
     
-    private void dfs(int index, int sum, int target, int[] candidates, List<Integer> comb, List<List<Integer>> results) {
-        if (sum == target) {
-            results.add(new ArrayList<>(comb));
-            return;
-        }
-        int lastElemIdx = comb.size();
-        for (int i = index; i < candidates.length; i++) {
-            if (i > index && candidates[i] == candidates[i - 1]) {
-                continue;
-            }
-            if (candidates[i] > target) {
-                return;
-            }
-            int temp = sum + candidates[i];
-            if (temp > target) {
-                return;
-            }
-            comb.add(candidates[i]);
-            dfs(i, temp, target, candidates, comb, results);
-            comb.remove(lastElemIdx);
+    private void dfs(int[] candidates, int idx, int crt, int target, List<Integer> comb, List<List<Integer>> res) {
+        if (crt == target) {
+            res.add(new ArrayList<>(comb));
+        } else {
+            if (idx < candidates.length - 1 && crt + candidates[idx + 1] <= target) dfs(candidates, idx + 1, crt, target, comb, res);
+            comb.add(candidates[idx]);
+            if (crt + candidates[idx] <= target) dfs(candidates, idx, crt + candidates[idx], target, comb, res);
+            comb.remove(comb.size() - 1);
         }
     }
 }
