f00344c97c89ce16cc8a9fd9e637ca48877ff828
ScottShao <neuscottshao@gmail.com>
2016-10-06 15:38:28 -0600

    [17. Letter Combinations of a Phone Number][Accepted]committed by LeetCode Extension

--- 17-Letter-Combinations-of-a-Phone-Number/solution.java
+++ 17-Letter-Combinations-of-a-Phone-Number/solution.java
@@ -1,26 +1,26 @@
 public class Solution {
-    private final char[][] lettersMap = new char[][]{{},{},{'a', 'b', 'c'}, {'d', 'e', 'f'}, {'g','h','i'}, {'j','k','l'}, {'m', 'n','o'}, {'p','q','r','s'}, {'t','u','v'}, {'w','x','y','z'}};
+    private final char[][] LETTER = new char[][]{{},{},{'a', 'b', 'c'}, {'d', 'e', 'f'}, {'g','h','i'}, {'j','k','l'}, {'m', 'n','o'}, {'p','q','r','s'}, {'t','u','v'}, {'w','x','y','z'}};
     public List<String> letterCombinations(String digits) {
-        List<String> results = new ArrayList<>();
-        if (digits == null || digits.length() == 0) {
-            return results;
-        }
-        char[] val = digits.toCharArray();
-        dfs(val, 0, new StringBuilder(), results);
-        return results;
+        List<String> res = new ArrayList<>();
+        if (digits == null) return res;
+        int len = digits.length();
+        if (len == 0) return res;
+        char[] chs = digits.toCharArray();
+        dfs(chs, 0, new StringBuilder(), res);
+        return res;
     }
     
-    private void dfs(char[] val, int crt, StringBuilder sb, List<String> results) {
-        if (crt >= val.length) {
-            results.add(sb.toString());
-            return;
-        }
-        char[] letters = lettersMap[val[crt] - '0'];
-        int len = sb.length();
-        for (char l : letters) {
-            sb.append(l);
-            dfs(val, crt + 1, sb, results);
-            sb.setLength(len);
+    private void dfs(char[] chs, int idx, StringBuilder sb, List<String> res) {
+        if (idx == chs.length) {
+            res.add(sb.toString());
+        } else {
+            char[] letters = LETTER[chs[idx] - '0'];
+            int sbLen = sb.length();
+            for (char ch : letters) {
+                sb.append(ch);
+                dfs(chs, idx + 1, sb, res);
+                sb.setLength(sbLen);
+            }
         }
     }
 }
