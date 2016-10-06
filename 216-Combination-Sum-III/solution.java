a79c0a971549b63bd67ceaaee504fb483a42d7ed
ScottShao <neuscottshao@gmail.com>
2016-10-06 16:04:32 -0600

    [216. Combination Sum III][Accepted]committed by LeetCode Extension

--- 216-Combination-Sum-III/solution.java
+++ 216-Combination-Sum-III/solution.java
@@ -1,24 +1,20 @@
 public class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> res = new ArrayList<>();
-        if (k * 9 < n || k * 1 > n) return res;
-        dfs(k, 0, n, 1, new ArrayList<>(), res);
+        dfs(k, n, 1, new ArrayList<>(), res);
         return res;
     }
     
-    private void dfs(int left, int sum, int n, int start, List<Integer> crt, List<List<Integer>> res) {
-        if (sum + left * 9 < n || sum + left * start > n) return;
-        int lastIdx = crt.size();
-        for (int i = start; i < 10; i++) {
-            if (left == 1) {
-                List<Integer> temp = new ArrayList<>(crt);
-                temp.add(n - sum);
-                res.add(temp);
-                return;
-            } else {
-                crt.add(i);
-                dfs(left - 1, sum + i, n, i + 1, crt, res);
-                crt.remove(lastIdx);
+    private void dfs(int k, int n, int idx, List<Integer> comb, List<List<Integer>> res) {
+        if (k * 9 < n || k * 1 > n) return;
+        if (n == 0) {
+            res.add(new ArrayList<>(comb));
+        } else {
+            if (idx < 9) dfs(k, n, idx + 1, comb, res);
+            if (idx <= 9) {
+                comb.add(idx);
+                dfs(k - 1, n - idx, idx + 1, comb, res);
+                comb.remove(comb.size() - 1);
             }
         }
     }
