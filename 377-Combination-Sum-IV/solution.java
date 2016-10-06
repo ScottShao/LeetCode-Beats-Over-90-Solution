46b3b88c433ff5d464cc7f442b7110c7e12e8742
ScottShao <neuscottshao@gmail.com>
2016-10-06 16:45:00 -0600

    [377. Combination Sum IV][Accepted]committed by LeetCode Extension

---
+++ 377-Combination-Sum-IV/solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        if (nums == null || target < 1) return 0;
+        int len = nums.length;
+        Arrays.sort(nums);
+        if (len == 0) return 0;
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int j = 0; j < len && nums[j] <= i; j++) {
+                dp[i] += dp[i - nums[j]];
+            }
+        }
+        return dp[target];
+    }
+}
