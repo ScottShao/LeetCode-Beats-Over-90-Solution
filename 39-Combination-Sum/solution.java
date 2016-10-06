8186a02a461eb26b4216e76d96865d98750ef235
ScottShao <neuscottshao@gmail.com>
2016-10-06 15:22:30 -0600

    [39. Combination Sum][Accepted]committed by LeetCode Extension

--- 39-Combination-Sum/solution.java
+++ 39-Combination-Sum/solution.java
@@ -5,17 +5,17 @@ public class Solution {
         int len = candidates.length;
         if (len == 0) return res;
         Arrays.sort(candidates);
-        dfs(candidates, 0, 0, target, new ArrayList<>(), res);
+        dfs(candidates, 0, target, new ArrayList<>(), res);
         return res;
     }
     
-    private void dfs(int[] candidates, int idx, int crt, int target, List<Integer> comb, List<List<Integer>> res) {
-        if (crt == target) {
+    private void dfs(int[] candidates, int idx, int target, List<Integer> comb, List<List<Integer>> res) {
+        if (0 == target) {
             res.add(new ArrayList<>(comb));
         } else {
-            if (idx < candidates.length - 1 && candidates[idx] + crt < target) dfs(candidates, idx + 1, crt, target, comb, res);
+            if (idx < candidates.length - 1 && candidates[idx] < target) dfs(candidates, idx + 1, target, comb, res);
             comb.add(candidates[idx]);
-            if (crt + candidates[idx] <= target) dfs(candidates, idx, crt + candidates[idx], target, comb, res);
+            if (candidates[idx] <= target) dfs(candidates, idx, target - candidates[idx], comb, res);
             comb.remove(comb.size() - 1);
         }
     }
